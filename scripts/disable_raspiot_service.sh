73d2139cdf75e38969630dd21850a62e1a58a0de
Tang <tanguy.bonneau@gmail.com>
2016-12-02 18:23:01 +0100

    Add enable/disable service scripts

---
+++ scripts/disable_raspiot_service.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Enable raspiot daemon at raspi startup
+#@see https://debian-administration.org/article/28/Making_scripts_run_at_boot_time_with_Debian
+#@see http://unix.stackexchange.com/a/164092
+#@see http://unix.stackexchange.com/a/106674
+
+function disable_on_sysvinit {
+    echo "Disabling raspiot on sysvinit..."
+    update-rc.d raspiot remove
+    echo Done
+    exit
+}
+
+function disable_on_systemctl {
+    echo "Disabling raspiot on systemctl..."
+    systemctl disable raspiot
+    echo Done
+    exit
+}
+
+#disable raspiot on systemctl
+[[ `systemctl` =~ -\.mount ]] && disable_on_systemctl
+#disable raspiot on sysvinit
+[[ -f /etc/init.d/cron && ! -h /etc/init.d/cron ]] && disable_on_sysvinit
---
+++ scripts/enable_raspiot_service.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Enable raspiot daemon at raspi startup
+#@see https://debian-administration.org/article/28/Making_scripts_run_at_boot_time_with_Debian
+#@see http://unix.stackexchange.com/a/164092
+#@see http://unix.stackexchange.com/a/106674
+
+function enable_on_sysvinit {
+    echo "Enabling raspiot on sysvinit..."
+    update-rc.d raspiot defaults
+    echo "Done"
+    exit
+}
+
+function enable_on_systemctl {
+    echo "Enabling raspiot on systemctl..."
+    systemctl enable raspiot
+    echo "Done"
+    exit
+}
+
+#enable raspiot on systemctl
+[[ `systemctl` =~ -\.mount ]] && enable_on_systemctl
+#enable raspiot on sysvinit
+[[ -f /etc/init.d/cron && ! -h /etc/init.d/cron ]] && enable_on_sysvinit
