bb3682f612e624c0f250327f0b9f4abd8c907c82
Tang <tanguy.bonneau@gmail.com>
2016-12-04 19:20:42 +0100

    Display ip at startup

--- raspiot/messageboard.py
+++ raspiot/messageboard.py
@@ -9,6 +9,7 @@ import time
 import task
 from ht1632c import HT1632C
 import uuid
+import socket
 
 __all__ = ['Messageboard']
 
@@ -73,6 +74,11 @@ class Messageboard(RaspIot):
         #init display task
         self.__display_task = task.BackgroundTask(self.__display_message, float(self._config['duration']))
         self.__display_task.start()
+
+        #display ip
+        #@see http://stackoverflow.com/a/1267524
+        ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        self.board.display_message('IP: %s' % str(ip))
 
     def stop(self):
         RaspIot.stop(self)
