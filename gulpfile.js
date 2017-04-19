f39c0765c7f850b47c79e052cea86fd10d4ce75d
tangb <tanguy.bonneau@gmail.com>
2017-04-19 12:26:48 +0200

    Introduce html production process (minify, checkcode)

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
+
+var source = './html';
+var destination = './dist';
+
+
+gulp.task('clean', function () {
+    return gulp.src(destination + '**', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('checkcode', function() {
+    return gulp.src([
+            source + '/js/**/*.js',
+            '!' + source + '/js/libs/**/*.js'
+        ])
+        .pipe(jshint())
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('minifyjs', function() {
+    return gulp.src([
+            source + '/js/**/*.js',
+            '!' + source + '/js/libs/**/*.js'
+        ])
+        .pipe(concat('raspiot.js'))
+        .pipe(rename('raspiot.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(destination + '/html/js/'));
+});
+
+gulp.task('dist', ['clean', 'checkcode', 'minifyjs']);
+
