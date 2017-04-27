4debe1a55d96dceaf4f646cceaf2aa2503c22659
tangb <tanguy.bonneau@gmail.com>
2017-04-27 12:07:33 +0200

    improve wpa_supplicant helper

--- raspiot/libs/wpasupplicantconf.py
+++ raspiot/libs/wpasupplicantconf.py
@@ -21,8 +21,8 @@ class WpaSupplicantConf():
     NETWORK_TYPE_WPA = 'wpa'
     NETWORK_TYPE_WPA2 = 'wpa2'
     NETWORK_TYPE_WEP = 'wep'
-    NETWORK_TYPE_UNSECURE = 'unsecure'
-    NETWORK_TYPES = [NETWORK_TYPE_WPA, NETWORK_TYPE_WPA2, NETWORK_TYPE_WEP, NETWORK_TYPE_UNSECURE]
+    NETWORK_TYPE_UNSECURED = 'unsecured'
+    NETWORK_TYPES = [NETWORK_TYPE_WPA, NETWORK_TYPE_WPA2, NETWORK_TYPE_WEP, NETWORK_TYPE_UNSECURED]
 
     def __init__(self):
         self.__fd = None
@@ -37,7 +37,7 @@ class WpaSupplicantConf():
         @raise Exception if file doesn't exist
         """
         if not os.path.exists(self.CONF):
-            raise Exception('wap_supplicant.conf file does not exist')
+            raise Exception('wpa_supplicant.conf file does not exist')
 
         self.__fd = open(self.CONF, mode)
         return self.__fd
@@ -61,16 +61,50 @@ class WpaSupplicantConf():
         groups = re.findall('network\s*=\s*\{\s*(.*?)\s*\}', content, re.S)
         for group in groups:
             ssid = None
+            scan_ssid = None
+            key_mgmt = None
+            hidden = False
+            network_type = self.NETWORK_TYPE_UNSECURED
             res = re.search('ssid="(.*?)"\s', group+'\n')
             if res:
                 ssid = res.group(1).strip()
-                networks.append(ssid)
+            res = re.search('scan_ssid="(.*?)"\s', group+'\n')
+            if res:
+                scan_ssid = res.group(1).strip()
+                if scan_ssid is not None and scan_ssid.isdigit() and scan_ssid=='1':
+                    hidden = True
+            res = re.search('key_mgmt="(.*?)"\s', group+'\n')
+            if res:
+                key_mgmt = res.group(1).strip()
+                if key_mgmt=='WPA-PSK':
+                    network_type = self.NETWORK_TYPE_WPA2
+                elif key_mgmt=='NONE':
+                    network_type = self.NETWORK_TYPE_WEP
+
+            networks.append({
+                'network': ssid,
+                'hidden': hidden,
+                'network_type': network_type
+            })
 
         return networks
 
-    def remove_network(self, network):
+    def get_network(self, network):
         """
-        Remove network from config
+        Get network config
+        @param network: network name
+        @return network config (dict) or None if network is not found
+        """
+        networks = self.get_networks()
+        for network_ in networks:
+            if network_['network']==network:
+                return network_
+
+        return None
+
+    def delete_network(self, network):
+        """
+        Delete network from config
         @param network: network name (ssid)
         """
         fd = self.__open()
@@ -103,36 +137,50 @@ class WpaSupplicantConf():
         Add new network in config file
         Password is automatically encrypted using wpa_passphrase
         @param network: network name (ssid)
-        @param network_type: type of network (WPA, WPA2, WEP, unsecure)
+        @param network_type: type of network (wpa, wpa, wep, unsecured)
         @param password: network password (string)
         @param hidden: hiddent network flag (bool)
         """
         #check params
         if network is None or len(network)==0:
             raise MissingParameter('Network parameter is missing')
-        if password is None or len(password)==0:
-            raise MissingParameter('Password parameter is missing')
         if network_type is None or len(network_type)==0:
             raise MissingParameter('Network_type parameter is missing')
         if network_type not in self.NETWORK_TYPES:
             raise InvalidParameter('Network_type "%s" does not exist (available: %s)' % (network_type, ','.join(self.NETWORK_TYPES)))
+        if network_type!=self.NETWORK_TYPE_UNSECURED and password is None or len(password)==0:
+            raise MissingParameter('Password parameter is missing')
 
         #check if network doesn't already exist
-        networks = self.get_networks()
-        if network in networks:
+        if self.get_network(network) is not None:
             raise InvalidParameter('Network "%s" is already configured')
     
         #get config to write with encrypted password and clear password removed
-        c = Console()
-        res = c.command('/usr/bin/wpa_passphrase "%s" "%s"' % (network, password))
-        if res['error'] or res['killed']:
-            raise Exception('Unable to encrypt password')
-        password = None
-        output = [line for line in res['output'] if not line.startswith('\t#psk=')]
+        if network_type!=self.NETWORK_TYPE_UNSECURED:
+            c = Console()
+            res = c.command('/usr/bin/wpa_passphrase "%s" "%s"' % (network, password))
+            if res['error'] or res['killed']:
+                raise Exception('Unable to encrypt password')
+            password = None
+            output = [line for line in res['output'] if not line.startswith('\t#psk=')]
 
-        #inject hidden param if necessary
-        if hidden:
-            output.insert('\tscan_ssid=1', 2)
+            #inject hidden param if necessary
+            if hidden:
+                output.insert(2, '\tscan_ssid=1')
+
+            #inject network type
+            if network_type in [self.NETWORK_TYPE_WPA, self.NETWORK_TYPE_WPA2]:
+                output.insert(2, '\tkey_mgmt=WPA-PSK')
+            elif network_type==self.NETWORK_TYPE_WEP:
+                output.insert(2, '\tkey_mgmt=NONE')
+
+        else:
+            #handle unsecured network
+            output = [
+                'network={',
+                '\tssid="%s"' % network,
+                '}'
+            ]
 
         #write new network config
         fd = self.__open(self.MODE_APPEND)
@@ -158,14 +206,14 @@ class WpaSupplicantConfTests_validConf(unittest.TestCase):
     def test_get_networks(self):
         networks = self.w.get_networks()
         self.assertEqual(len(networks), 2)
-        self.assertEqual(networks[0], 'mynetwork1')
-        self.assertEqual(networks[1], 'mynetwork2')
+        self.assertEqual(networks[0]['network'], 'mynetwork1')
+        self.assertEqual(networks[1]['network'], 'mynetwork2')
 
     def test_add_network(self):
         self.assertTrue(self.w.add_network('mynetwork3', 'wpa', 'mypassword3'))
         networks = self.w.get_networks()
         self.assertEqual(len(networks), 3)
-        self.assertEqual(networks[2], 'mynetwork3')
+        self.assertEqual(networks[2]['network'], 'mynetwork3')
 
     def test_add_existing_network(self):
         self.assertRaises(Exception, self.w.add_network, 'mynetwork2', 'wpa', 'mypassword2')
@@ -182,14 +230,14 @@ class WpaSupplicantConfTests_validConf(unittest.TestCase):
         self.assertRaises(InvalidParameter, self.w.add_network, 'mynetwork3', 'wpa6', 'mypassword2')
         self.assertRaises(InvalidParameter, self.w.add_network, 'mynetwork2', 'wpa2', 'mypassword2')
 
-    def test_remove_existing_network(self):
-        self.assertTrue(self.w.remove_network('mynetwork2'))
+    def test_delete_existing_network(self):
+        self.assertTrue(self.w.delete_network('mynetwork2'))
         self.assertEqual(len(self.w.get_networks()), 1)
-        self.assertTrue(self.w.remove_network('mynetwork1'))
+        self.assertTrue(self.w.delete_network('mynetwork1'))
         self.assertEqual(len(self.w.get_networks()), 0)
 
-    def test_remove_unknown_network(self):
-        self.assertRaises(CommandError, self.w.remove_network, 'network666')
+    def test_delete_unknown_network(self):
+        self.assertRaises(CommandError, self.w.delete_network, 'network666')
         self.assertEqual(len(self.w.get_networks()), 2)
 
 
@@ -213,5 +261,5 @@ class WpaSupplicantConfTests_emptyConf(unittest.TestCase):
         self.assertTrue(self.w.add_network('mynetwork3', 'wpa', 'mypassword3'))
         networks = self.w.get_networks()
         self.assertEqual(len(networks), 1)
-        self.assertEqual(networks[0], 'mynetwork3')
+        self.assertEqual(networks[0]['network'], 'mynetwork3')
 
