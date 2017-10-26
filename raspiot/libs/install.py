1f85988d6a5c66bb568431a3edc0a6fbd5f6f3b4
Tangb <tanguy.bonneau@gmail.com>
2017-10-26 11:53:35 +0200

    Add install library with associated testu

---
+++ raspiot/libs/install.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+import logging
+import time
+import os
+from raspiot.libs.console import EndlessConsole
+
+class Install():
+    """
+    Install helper
+    This class helps you to install different kind of files:
+     - deb files using dpkg
+     - tar.gz using tar
+     - zip using unzip
+    """
+
+    STATUS_IDLE = 0
+    STATUS_PROCESSING = 1
+    STATUS_ERROR = 2
+    STATUS_DONE = 3
+    STATUS_CANCELED = 4
+
+    def __init__(self, status_callback):
+        """
+        Constructor
+
+        Args:
+            status_callback (function): status callback. Params: status
+        """
+        #logger
+        self.logger = logging.getLogger(self.__class__.__name__)
+        #self.logger.setLevel(logging.DEBUG)
+
+        #members
+        self.__console = None
+        self.__cancel = False
+        self.status = self.STATUS_IDLE
+        self.status_callback = status_callback
+        self.stdout = []
+        self.stderr = []
+
+    def cancel(self):
+        """
+        Cancel current install
+        """
+        self.status = self.STATUS_CANCELED
+        if self.__console:
+            self.__console.stop()
+
+    def get_status(self):
+        """
+        Return current installation status
+
+        Return:
+            dict: status::
+                {
+                    status (string): current status
+                    stdout (list): list of messages received on console
+                    stderr (list): list of messages received on stderr
+                }
+        """
+        return {
+            'status': self.status,
+            'stdout': self.stdout,
+            'stderr': self.stderr
+        }
+
+    def __reset_status(self, status):
+        """
+        Reset status
+
+        Args:
+            status (int): status to set (use self.STATUS_XXX)
+        """
+        self.status = status
+        self.stdout = []
+        self.stderr = []
+
+    def __callback_end(self):
+        """
+        End of process callback
+        """
+        self.logger.debug('End of command')
+
+        #update status if necessary
+        if self.status!=self.STATUS_ERROR:
+            self.status = self.STATUS_DONE
+
+        #send for the last time current status
+        self.status_callback(self.get_status())
+
+    def __callback_quiet(self, stdout, stderr):
+        """
+        Quiet output. Does nothing
+        """
+        pass
+
+    def refresh_packages(self):
+        """
+        Refresh packages list
+        """
+        if self.status==self.STATUS_PROCESSING:
+            raise Exception(u'Installer is already processing')
+
+        #update status
+        self.__reset_status(self.STATUS_PROCESSING)
+
+        #refresh packages
+        command = u'/usr/bin/aptitude update'
+        self.logger.debug('Command: %s' % command)
+        self.__console = EndlessConsole(command, self.__callback_quiet, self.__callback_end)
+        self.__console.start()
+
+    def __callback_package(self, stdout, stderr):
+        """
+        Package install callback
+
+        Args:
+            stdout (list): console stdout
+            stderr (list): console stderr
+        """
+        #append current stdout/stderr
+        if stdout is not None:
+            self.stdout.append(stdout)
+        if stderr is not None:
+            self.status = self.STATUS_ERROR
+            self.stderr.append(stderr)
+
+        #send status to caller callback
+        self.status_callback(self.get_status())
+
+    def install_package(self, package_name):
+        """
+        Install package using aptitude
+
+        Args:
+            package_name (string): package name to install
+        """
+        if self.status==self.STATUS_PROCESSING:
+            raise Exception(u'Installer is already processing')
+
+        #update status
+        self.__reset_status(self.STATUS_PROCESSING)
+
+        #install deb
+        command = u'/usr/bin/aptitude install -y "%s"' % package_name
+        self.logger.debug('Command: %s' % command)
+        self.__console = EndlessConsole(command, self.__callback_package, self.__callback_end)
+        self.__console.start()
+
+    def uninstall_package(self, package_name, purge=False):
+        """
+        Install package using aptitude
+
+        Args:
+            package_name (string): package name to install
+            purge (bool): purge package (remove config files)
+        """
+        if self.status==self.STATUS_PROCESSING:
+            raise Exception(u'Installer is already processing')
+
+        #update status
+        self.__reset_status(self.STATUS_PROCESSING)
+
+        #install deb
+        action = 'remove'
+        if purge:
+            action = 'purge'
+        command = u'/usr/bin/aptitude %s -y "%s"' % (action, package_name)
+        self.logger.debug('Command: %s' % command)
+        self.__console = EndlessConsole(command, self.__callback_package, self.__callback_end)
+        self.__console.start()
+
+    def __callback_deb(self, stdout, stderr):
+        """
+        Deb install callback
+
+        Args:
+            stdout (list): console stdout
+            stderr (list): console stderr
+        """
+        #append current stdout/stderr
+        if stdout is not None:
+            #end of command ends with broken pipe on yes execution, this is normal. Drop this unimportant message
+            #https://stackoverflow.com/questions/20573282/hudson-yes-standard-output-broken-pipe
+            if stdout.find(u'yes')==-1 and stdout.lower().find(u'broken pipe')==-1:
+                self.stdout.append(stdout)
+        if stderr is not None:
+            self.status = self.STATUS_ERROR
+            self.stderr.append(stderr)
+
+        #send status to caller callback
+        self.status_callback(self.get_status())
+
+    def install_deb(self, deb):
+        """
+        Install .deb file using dpkg
+        """
+        if self.status==self.STATUS_PROCESSING:
+            raise Exception(u'Installer is already processing')
+
+        #update status
+        self.__reset_status(self.STATUS_PROCESSING)
+
+        #install deb (and dependencies)
+        command = u'/usr/bin/yes | /usr/bin/dpkg -i "%s" && /usr/bin/apt-get install -f && /usr/bin/yes | /usr/bin/dpkg -i "%s"' % (deb, deb)
+        self.logger.debug('Command: %s' % command)
+        self.__console = EndlessConsole(command, self.__callback_deb, self.__callback_end)
+        self.__console.start()
+
+    def __callback_archive(self, stdout, stderr):
+        """
+        Deb install callback
+
+        Args:
+            stdout (list): console stdout
+            stderr (list): console stderr
+        """
+        #append current stdout/stderr
+        if stdout is not None:
+            self.stdout.append(stdout)
+        if stderr is not None:
+            self.status = self.STATUS_ERROR
+            self.stderr.append(stderr)
+
+        #send status to caller callback
+        self.status_callback(self.get_status())
+        
+    def install_archive(self, archive, install_path):
+        """
+        Install archive (.tar.gz, .zip) to specified install path
+
+        Args:
+            archive (string): archive full path
+            install_path (string): installation fullpath directory
+        """
+        if self.status==self.STATUS_PROCESSING:
+            raise Exception(u'Installer is already processing')
+
+        #check params
+        if archive is None or len(archive.strip())==0:
+            raise Exception('Parameter "archive" is missing')
+        if install_path is None or len(install_path.strip())==0:
+            raise Exception('Parameter "install_path" is missing')
+        if not os.path.exists(archive):
+            raise Exception('Archive does not exist')
+
+        #create output dir if it isn't exist
+        if not os.path.exists(install_path):
+            os.makedirs(install_path)
+
+        #update status
+        self.__reset_status(self.STATUS_PROCESSING)
+
+        #get archive decompressor according to archive extension
+        command = None
+        dummy, ext = os.path.splitext(archive)
+        if ext=='.gz':
+            _, ext = os.path.splitext(dummy)
+            if ext=='.tar':
+                command = '/bin/tar xzvf "%s" -C "%s"' % (archive, install_path)
+
+        elif ext=='.zip':
+            command = '/usr/bin/unzip "%s" -d "%s"' % (archive, install_path)
+
+        #execute command
+        if command is None:
+            raise Exception('File format not supported. Only zip and tar.gz supported.')
+        else:
+            self.logger.debug('Command: %s' % command)
+            self.__console = EndlessConsole(command, self.__callback_archive, self.__callback_end)
+            self.__console.start()
+
+
---
+++ raspiot/tests/test_install.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from raspiot.libs.install import Install
+from raspiot.libs.console import Console
+import unittest
+import os
+import time
+import logging
+import shutil
+
+logging.basicConfig(level=logging.INFO, format=u'%(asctime)s %(name)s %(levelname)s : %(message)s')
+
+class InstallTests(unittest.TestCase):
+    def setUp(self):
+        self.running = True
+        self.deb = None
+        self.status = Install.STATUS_IDLE
+        self.stdout = []
+        self.stderr = []
+        self.i = Install(self.callback)
+        self.archive = None
+        self.directory = None
+
+    def tearDown(self):
+        #remove files
+        if self.deb:
+            os.remove(self.deb)
+        if self.archive:
+            os.remove(self.archive)
+
+        #remove directories
+        if self.directory:
+            shutil.rmtree(self.directory)
+
+    def callback(self, status):
+        if status[u'status']==Install.STATUS_ERROR or status[u'status']==Install.STATUS_DONE:
+            self.running = False
+            self.status = status[u'status']
+            self.stdout = status[u'stdout']
+            self.stderr = status[u'stderr']
+
+    def test_install_deb(self):
+        self.running = True
+
+        #download package that will be installed
+        c = Console()
+        c.command(u'aptitude download dhex', 60)
+        self.deb = None
+        for root, dirs, files in os.walk(u'.'):
+            for file in files:
+                if file.startswith(u'dhex') and file.endswith(u'.deb'):
+                    self.deb = os.path.join(root, file)
+                    break
+        self.assertIsNotNone(self.deb)
+
+        #process install
+        self.i.install_deb(self.deb)
+        while self.running:
+            time.sleep(0.5)
+
+        self.assertEqual(Install.STATUS_DONE, self.status)
+        self.assertEqual(0, len(self.stderr))
+        self.assertNotEqual(0, len(self.stdout))
+
+    def test_refresh_packages(self):
+        self.running = True
+
+        #process refresh
+        self.i.refresh_packages()
+        while self.running:
+            time.sleep(0.5)
+
+        self.assertEqual(Install.STATUS_DONE, self.status)
+
+    def test_already_running(self):
+        self.running = True
+
+        #process refresh
+        self.i.refresh_packages()
+            
+        #launch again new process
+        self.assertRaises(Exception, self.i.refresh_packages)
+
+        #wait end of first process
+        while self.running:
+            time.sleep(0.5)
+
+    def test_cancel(self):
+        self.running = True
+
+        #process refresh
+        self.i.refresh_packages()
+        time.sleep(2.0)
+        status = self.i.get_status()
+        self.assertEqual(Install.STATUS_PROCESSING, status['status'])
+
+        self.i.cancel()
+        status = self.i.get_status()
+        self.assertEqual(Install.STATUS_CANCELED, status['status'])
+
+    def test_install_package(self):
+        self.running = True
+
+        #process install
+        self.i.install_package('dhex')
+        while self.running:
+            time.sleep(0.5)
+
+        self.assertEqual(Install.STATUS_DONE, self.status)
+        self.assertEqual(0, len(self.stderr))
+        self.assertNotEqual(0, len(self.stdout))
+
+        c = Console()
+        res = c.command('dpkg -l | grep dhex')
+        self.assertFalse(res['error'])
+        self.assertTrue(res['stdout'][0].strip().startswith('ii'))
+
+    def test_uninstall_package(self):
+        self.running = True
+
+        #process uninstall
+        self.i.uninstall_package('dhex')
+        while self.running:
+            time.sleep(0.5)
+        
+        self.assertEqual(Install.STATUS_DONE, self.status)
+        self.assertEqual(0, len(self.stderr))
+        self.assertNotEqual(0, len(self.stdout))
+
+        c = Console()
+        res = c.command('dpkg -l | grep dhex')
+        self.assertFalse(res['error'])
+        self.assertNotEqual(0, len(res['stdout']))
+        self.assertTrue(res['stdout'][0].strip().startswith('rc'))
+
+    def test_uninstall_package_purge(self):
+        self.running = True
+
+        #process uninstall
+        self.i.uninstall_package('dhex', purge=True)
+        while self.running:
+            time.sleep(0.5)
+        
+        self.assertEqual(Install.STATUS_DONE, self.status)
+        self.assertEqual(0, len(self.stderr))
+        self.assertNotEqual(0, len(self.stdout))
+
+        c = Console()
+        res = c.command('dpkg -l | grep dhex')
+        self.assertFalse(res['error'])
+        self.assertEqual(0, len(res['stdout']))
+        self.assertEqual(0, len(res['stderr']))
+
+    def test_install_archive_invalid_archive(self):
+        self.running = True
+
+        #process install
+        self.assertRaises(Exception, self.i.install_archive, 'dummy_archive.tar.gz', '.')
+
+    def test_install_archive_invalid_format(self):
+        self.running = True
+
+        self.archive = 'dummy_archive.7z'
+        f = open(self.archive, 'w')
+        f.write('dummy')
+        f.close()
+        time.sleep(.5)
+
+        #process install
+        self.assertRaises(Exception, self.i.install_archive, self.archive, '.')
+
+    def test_install_archive(self):
+        self.running = True
+
+        self.archive = 'dummy_archive.tar.gz'
+        self.directory = '/tmp/test_install'
+        c = Console()
+        res = c.command('tar czvf "%s" test*.py' % self.archive)
+        self.assertFalse(res['error'])
+
+        #process install
+        self.i.install_archive(self.archive, self.directory)
+        while self.running:
+            time.sleep(0.5)
+
+        self.assertEqual(Install.STATUS_DONE, self.status)
+        self.assertEqual(0, len(self.stderr))
+        self.assertTrue(os.path.exists(self.directory))
+        path, dirs, files = os.walk(self.directory).next()
+        self.assertNotEqual(0, len(files))
+        
+        
