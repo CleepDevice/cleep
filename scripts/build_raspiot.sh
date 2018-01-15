75234dba666abfcc240eeb8a814f30393e669956
Tanguy BONNEAU <tanguy.bonneau@gmail.com>
2018-01-15 21:03:48 +0000

    Add build_raspiot.sh script

---
+++ scripts/build_raspiot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#script that build raspiot deb package
+
+CUR_DIR=`pwd`
+
+#add log if --newversion specified in command line
+#dch -i
+
+#build raspiot application
+cd /root/raspiot
+debuild -us -uc 
+debuild clean
+rm -rf raspiot.egg-info
+rm -rf pyraspiot.egg-info/
+
+cd $CUR_DIR
