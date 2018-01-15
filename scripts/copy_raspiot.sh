8f14eba8b887363613a055e56efc68942778193f
Tanguy <tanguy.bonneau@gmail.com>
2018-01-15 21:46:42 +0000

    Add command to copy files in stretch

--- scripts/copy_raspiot.sh
+++ scripts/copy_raspiot.sh
@@ -4,9 +4,9 @@
 
 cd /root/raspiot/
 #raspbian jessie (also need to add manually link to new files to /usr/lib/python2.7/dist-packages/raspiot/
-cp -a raspiot/* /usr/share/pyshared/raspiot/.
+#cp -a raspiot/* /usr/share/pyshared/raspiot/.
 #raspbian stretch
-cp -a raspiot/* /usr/share/pyshared/raspiot/.
+cp -a raspiot/* /usr/lib/python2.7/dist-packages/raspiot/.
 cp -a html /opt/raspiot/.
 cp -a bin/raspiot /usr/bin/raspiot
 cp -a medias/sounds/* /opt/raspiot/sounds/
