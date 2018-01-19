a7f128caa1ca8a73078e1f153bc56c9f497004e0
Tanguy <tanguy.bonneau@gmail.com>
2018-01-19 23:00:35 +0000

    Handle driver install/uninstall

--- raspiot/libs/drivers/respeaker2mic/respeaker2mic.py
+++ raspiot/libs/drivers/respeaker2mic/respeaker2mic.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from raspiot.libs.console import AdvancedConsole
-from raspiot.utils import InvalidParameter, CommandError
+from raspiot.libs.console import Console
+from raspiot.utils import InvalidParameter, MissingParameter
 import raspiot.libs.drivers.respeaker2mic.apa102 as apa102
 import logging
+import os
+import shutil
 
 class Respeaker2mic(AdvancedConsole):
     """
@@ -21,6 +23,10 @@ class Respeaker2mic(AdvancedConsole):
     COLOR_MAGENTA = [255, 0, 255]
 
     LED_OFF = COLOR_BLACK
+
+    RESPEAKER_REPO = u'https://github.com/respeaker/seeed-voicecard.git'
+    TMP_DIR = u'/tmp/respeaker'
+    DRIVER_PATH = u'/boot/overlays/seeed-2mic-voicecard.dtbo'
     
     def __init__(self):
         """
@@ -37,6 +43,21 @@ class Respeaker2mic(AdvancedConsole):
             [0,0,0],
             [0,0,0]
         ]
+        self.__driver_process = None
+        self.__process_status_callback = None
+        self.__process_terminated_callback = None
+
+    def is_installing(self):
+        """
+        Return True if driver process (install or uninstall) is running
+
+        Return:
+            bool: True if driver process is running
+        """
+        if self.__driver_process is None:
+            return False
+
+        return True
 
     def is_installed(self):
         """
@@ -45,16 +66,164 @@ class Respeaker2mic(AdvancedConsole):
         Return:
             bool: True if driver already installed
         """
-        pass
+        #check if drivers installed
+        if os.path.exists(self.DRIVER_PATH):
+            return True
 
-    def install_driver(self):
+        return False
+
+    def __process_status_callback(self, stdout, stderr):
         """
-        Install respeaker 2mic driver
+        Called when running process received something on stdout/stderr
+
+        Args:
+            stdout (string): command stdout string
+            stderr (string): command stderr string
+        """
+        #log for debug installation logs
+        if stdout:
+            self.logger.debug(u'Driver installation stdout: %s' % stdout)
+        if stderr:
+            self.logger.debug(u'Driver installation stderr: %s' % stderr)
+
+        #callback
+        self.__process_status_callback(stdout, stderr)
+
+    def __process_terminated_callback(self, return_code, killed):
+        """
+        Called when running process is terminated
+
+        Args:
+            return_code (string): command return code
+            killed (bool): if True command was killed
+        """
+        #clean everything
+        if os.path.exists(self.TMP_DIR):
+            shutil.rmtree(self.TMP_DIR)
+
+        #callback
+        self.__process_terminated_callback(return_code, killed)
+
+        #reset
+        self.__driver_process = None
+        self.__process_status_callback = None
+        self.__process_terminated_callback = None
+
+    def __prepare_driver_process(self):
+        """
+        Prepare driver process (install or uninstall).
+        It downloads source and make sure everything is clean before starting
+
+        Raise:
+            Exception
+        """
+        #make sure directory does not exist
+        if os.path.exists(self.TMP_DIR):
+            shutil.rmtree(self.TMP_DIR)
+
+        #get sources
+        console = Console()
+        res = console.command(u'/usr/bin/git "%s" /tmp/respeaker-driver' % self.RESPEAKER_REPO, timeout=30)
+        if res[u'error'] or res[u'killed']:
+            self.logger.error(u'Error occured during git command: %s' % res)
+            raise Exception(u'Unable to install respeaker driver: respeaker repository seems no available.')
+        if not os.path.exists(os.path.join(self.TMP_DIR, u'uninstall.sh')):
+            self.logger.error('Install.sh script does not exists. Unable to install respeaker 2mic driver')
+            raise Exception(u'Unable to install respeaker driver: install script does not exist.')
+
+    def install_driver(self, install_status_callback, install_terminated_callback):
+        """
+        Install respeaker 2mic driver. At end of installation device reboot is required
+
+        Args:
+            install_status_callback (function): callback during installation process (useful to save output) (args: stdout(string), stderr(string))
+            install_terminated_callback (function): callback when installation is over (args: command return code(string), killed status (bool))
 
         Return:
-            bool: True if driver installed successfully
+            bool: True if driver installation is running
         """
-        pass
+        #check params
+        if install_status_callback is None:
+            raise MissingParameter(u'Parameter install_status_callback is missing')
+        if not callable(install_status_callback):
+            raise InvalidParameter(u'Parameter install_status_callback must be a callback')
+        if install_terminated_callback is None:
+            raise MissingParameter(u'Parameter install_terminated_callback is missing')
+        if not callable(install_terminated_callback):
+            raise InvalidParameter(u'Parameter install_terminated_callback must be a callback')
+        if self.__driver_process is not None:
+            raise Exception(u'Driver process is already running. Stop it before launching installation')
+
+        #save parameters
+        self.__process_status_callback = install_status_callback
+        self.__process_terminated_callback = install_terminated_callback
+
+        #prepare install
+        self.__prepare_driver_process()
+        if not os.path.exists(os.path.join(self.TMP_DIR, u'install.sh')):
+            self.logger.error('Install.sh script does not exists. Unable to install respeaker 2mic driver')
+            raise Exception(u'Unable to install respeaker driver: install script does not exist.')
+
+        #build and install driver
+        command = u'%s 2mic' % (os.path.join(self.TMP_DIR, u'install.sh'))
+        self.logger.debug('Respeaker driver install command: %s' % command)
+        self.__driver_process = EndlessConsole(command, self.__process_status_callback, self.__process_terminated_callback)
+        self.__driver_process.start()
+
+    def uninstall_driver(self, uninstall_status_callback, uninstall_terminated_callback):
+        """
+        Uninstall respeaker 2mic driver. At end of uninstallation device reboot is required
+
+        Args:
+            uninstall_status_callback (function): callback during uninstallation process (useful to save output) (args: stdout(string), stderr(string))
+            uninstall_terminated_callback (function): callback when uninstallation is over (args: command return code(string), killed status (bool))
+
+        Return:
+            bool: True if driver installation is running
+        """
+        #check params
+        if uninstall_status_callback is None:
+            raise MissingParameter(u'Parameter uninstall_status_callback is missing')
+        if not callable(uninstall_status_callback):
+            raise InvalidParameter(u'Parameter uninstall_status_callback must be a callback')
+        if uninstall_terminated_callback is None:
+            raise MissingParameter(u'Parameter uninstall_terminated_callback is missing')
+        if not callable(uninstall_terminated_callback):
+            raise InvalidParameter(u'Parameter uninstall_terminated_callback must be a callback')
+        if self.__driver_process is not None:
+            raise Exception(u'Driver process is already running. Stop it before launching uninstallation')
+
+        #save parameters
+        self.__process_status_callback = uninstall_status_callback
+        self.__process_terminated_callback = uninstall_terminated_callback
+
+        #prepare uninstall
+        self.__prepare_driver_process()
+        if not os.path.exists(os.path.join(self.TMP_DIR, u'uninstall.sh')):
+            self.logger.error('Uninstall.sh script does not exists. Unable to install respeaker 2mic driver')
+            raise Exception(u'Unable to uninstall respeaker driver: uninstall script does not exist.')
+
+        #uninstall driver
+        command = u'%s 2mic' % (os.path.join(self.TMP_DIR, u'uninstall.sh'))
+        self.logger.debug('Respeaker driver uninstall command: %s' % command)
+        self.__driver_process = EndlessConsole(command, self.__process_status_callback, self.__process_terminated_callback)
+        self.__driver_process.start()
+
+    def stop_driver_process(self):
+        """
+        Stop running driver process (install or uninstall) if running
+
+        Return:
+            bool: True if running process was stopped, False if no process was running
+        """
+        if self.__driver_process is not None:
+            self.__driver_process.kill()
+            self.__driver_process = None
+
+            return True
+
+        #no driver process running
+        return False
 
     def configure_button(self, send_command_callback):
         """
@@ -64,7 +233,7 @@ class Respeaker2mic(AdvancedConsole):
             uuid: button device uuid, useful to catch its event on module event_received function
 
         Raise:
-            CommandError
+            Exception
         """
 	#configure gpio
         params = {
@@ -76,7 +245,7 @@ class Respeaker2mic(AdvancedConsole):
         }
         resp_gpio = send_command_callback(u'add_gpio', u'gpios', { u'mode':u'out', u'keep':False, u'reverted':False})
         if resp_gpio[u'error']:
-            raise CommandError(resp_gpio[u'message'])
+            raise Exception(resp_gpio[u'message'])
 
         return resp_gpio[u'data'][u'uuid']
 
