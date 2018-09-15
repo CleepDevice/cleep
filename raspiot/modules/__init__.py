9d9fd3be102fcf25e21a48d1e84011e8dccd9694
Tang <tanguy.bonneau@gmail.com>
2018-09-15 22:23:08 +0200

    Remove modules directory

--- raspiot/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['audio', 'cleepbus', 'network', 'system', 'parameters']
-
-from raspiot.modules.audio import *
-from raspiot.modules.cleepbus import *
-from raspiot.modules.network import *
-from raspiot.modules.system import *
-from raspiot.modules.parameters import *
-
