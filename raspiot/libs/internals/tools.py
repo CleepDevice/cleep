746ef545b518f93cfc8ca1b8ce0f1dfa32f7e199
Tangb <tanguy.bonneau@gmail.com>
2019-04-14 22:14:24 +0200

    External trace logging installation and use it in raspiot bin

--- raspiot/libs/internals/tools.py
+++ raspiot/libs/internals/tools.py
@@ -6,6 +6,7 @@ from passlib.utils import pbkdf2
 import base64
 import io
 import os
+import logging
 
 from raspiot.libs.internals import __all__ as internals_libs
 #from raspiot.libs.externals import __all__ as externals_libs
@@ -13,6 +14,22 @@ from raspiot.libs.drivers import __all__ as drivers_libs
 from raspiot.libs.configs import __all__ as configs_libs
 from raspiot.libs.commands import __all__ as commands_libs
 
+def install_trace_logging_level():
+    """ 
+    Install custom log level TRACE for library debugging principaly
+    Credits https://gist.github.com/numberoverzero/f803ebf29a0677b6980a5a733a10ca71
+    """
+    level = logging.TRACE = logging.DEBUG - 5 
+
+    def log_logger(self, message, *args, **kwargs):
+        if self.isEnabledFor(level):
+            self._log(level, message, args, **kwargs)
+    logging.getLoggerClass().trace = log_logger
+
+    def log_root(msg, *args, **kwargs):
+        logging.log(level, msg, *args, **kwargs)
+    logging.addLevelName(level, "TRACE")
+    logging.trace = log_root
 
 DBM_TO_PERCENT = {-1:100, -2:100, -3:100, -4:100, -5:100, -6:100, -7:100, -8:100, -9:100, -10:100, -11:100, -12:100, -13:100, -14:100, -15:100, -16:100, -17:100, -18:100, -19:100, -20:100, -21:99, -22:99, -23:99, -24:98, -25:98, -26:98, -27:97, -28:97, -29:96, -30:96, -31:95, -32:95, -33:94, -34:93, -35:93, -36:92, -37:91, -38:90, -39:90, -40:89, -41:88, -42:87, -43:86, -44:85, -45:84, -46:83, -47:82, -48:81, -49:80, -50:79, -51:78, -52:76, -53:75, -54:74, -55:73, -56:71, -57:70, -58:69, -59:67, -60:66, -61:64, -62:63, -63:61, -64:60, -65:58, -66:56, -67:55, -68:53, -69:51, -70:50, -71:48, -72:46, -73:44, -74:42, -75:40, -76:38, -77:36, -78:34, -79:32, -80:30, -81:28, -82:26, -83:24, -84:22, -85:20, -86:17, -87:15, -88:13, -89:10, -90:8, -91:6, -92:3, -93:1, -94:1, -95:1, -96:1, -97:1, -98:1, -99:1, -100:1}
 
