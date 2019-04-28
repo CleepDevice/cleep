fb0c686e38c884f0de31f929400437da914f1f90
Tangb <tanguy.bonneau@gmail.com>
2019-04-29 00:15:54 +0200

    Update python libraries

--- scripts/preinst.sh
+++ scripts/preinst.sh
@@ -13,7 +13,7 @@ checkResult() {
 
 #install system dependencies
 #we install scipy and numpy from raspiot repositories because it's too long (and problematic) to build these libraries on raspi
-apt-get update
+apt-get -y update
 apt-get install -y python-dev libffi-dev libssl-dev python-scipy python-numpy
 checkResult $? 0
 apt-get clean -y
@@ -33,7 +33,7 @@ echo ""
 
 #install python dependencies
 echo "Installing python dependencies..."
-pip install --trusted-host pypi.python.org "future==0.16.0" "six==1.11.0" "gevent==1.2.2" "bottle==0.12.13" "pyre-gevent==0.2.1" "netifaces==0.10.7" "uptime==3.0.1" "urllib3==1.22" "psutil==5.4.6" "passlib==1.7.1" "simplejson==3.15.0" "raven==6.9.0" "pytz==2018.4" "reverse-geocode==1.3" "timezonefinder==3.0.1" "tzlocal==1.5.1"
+pip install --no-cache-dir --trusted-host pypi.python.org "future==0.17.1" "six==1.12.0" "gevent==1.4.0" "bottle==0.12.16" "pyre-gevent==0.2.1" "netifaces==0.10.9" "uptime==3.0.1" "urllib3==1.24.2" "psutil==5.6.1" "passlib==1.7.1" "simplejson==3.16.0" "raven==6.10.0" "pytz==2019.1" "reverse-geocode==1.3" "timezonefinder==3.4.2" "tzlocal==1.5.1"
 checkResult $? 0
 echo "Done"
 echo ""
