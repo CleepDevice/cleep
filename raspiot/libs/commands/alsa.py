a95a3538f7dca061552937c8b3c1252e14496e5f
Tangb <tanguy.bonneau@gmail.com>
2019-05-12 23:29:36 +0200

    Major changes on alsa lib

--- raspiot/libs/commands/alsa.py
+++ raspiot/libs/commands/alsa.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from raspiot.libs.internals.console import AdvancedConsole
-from raspiot.libs.configs.asoundrc import Asoundrc
-from raspiot.libs.drivers.driver import Driver
-from raspiot.libs.drivers.audiodriver import AudioDriver
 from raspiot.utils import CommandError, MissingParameter, InvalidParameter
 import logging
 import re
@@ -16,10 +13,6 @@ class Alsa(AdvancedConsole):
     Alsa commands helper (aplay, arecord, amixer).
     """
     
-    #OUTPUT_TYPE_JACK = 1
-    #OUTPUT_TYPE_HDMI = 2
-    #OUTPUT_TYPE_EXTERNAL = 3 #external soundcard like hifiberry, respeaker...
-
     FORMAT_S16LE = u'S16_LE'
     FORMAT_S24LE = u'S24_LE'
     FORMAT_S32LE = u'S32_LE'
@@ -32,61 +25,20 @@ class Alsa(AdvancedConsole):
     RATE_44K = 44100
     RATE_48K = 48000
 
-    __KEY_PLAYBACK = u'playback_volume'
-    __KEY_CAPTURE = u'capture_volume'
-
-    #Devices profiles:
-    #An entry is composed of:
-    # - name (string): the device name as returned by alsa commands (aplay -l)
-    # - type (int): the device type (see above OUTPUT_TYPE_XXX, it should be 3 for new devices)
-    # - playback_volume (string): alsa control for the playback volume 
-    # - playback_volume_data (tuple): key and pattern to get playback volume value
-    # - capture_volume (string): alsa control for the capture volume 
-    # - capture_volume_data (tuple): key and pattern to get capture volume value
-    #DEVICES_PROFILES = {
-    #    u'bcm2835 ALSA': {
-    #        u'name': u'bcm2835 ALSA',
-    #        u'type': 1,
-    #        u'playback_volume': u'PCM',
-    #        u'playback_volume_data': (u'Mono', r'\[(\d*)%\]'),
-    #        u'capture_volume': None,
-    #        u'capture_volume_data': None
-    #    },
-    #    u'bcm2835 IEC958/HDMI': {
-    #        u'name': u'bcm2835 IEC958/HDMI',
-    #        u'type': 2,
-    #        u'playback_volume': u'PCM',
-    #        u'playback_volume_data': (u'Mono', r'\[(\d*)%\]'),
-    #        u'capture_volume': None,
-    #        u'capture_volume_data': None
-    #    },
-    #    u'seeed-2mic-voicecard': {
-    #        u'name': u'seeed-2mic-voicecard',
-    #        u'type': 3,
-    #        u'playback_volume': u'Playback',
-    #        u'playback_volume_data': (u'Front Left', r'\[(\d*)%\]'),
-    #        u'capture_volume': u'Capture',
-    #        u'capture_volume_data': (u'Front Left', r'\[(\d*)%\]')
-    #    }
-    #}
-
     SIMPLE_MIXER_CONTROL = u'Simple mixer control'
 
-    def __init__(self, drivers, cleep_filesystem):
+    CGET = u'cget'
+    CSET = u'cset'
+
+    def __init__(self):
         """
         Constructor
-
-        Args:
-            drivers (Drivers): Drivers instance (holds driver definition)
-            cleep_filesystem (CleepFilesystem): CleepFilesystem instance
         """
         AdvancedConsole.__init__(self)
 
         #members
         self.logger = logging.getLogger(self.__class__.__name__)
         #self.logger.setLevel(logging.DEBUG)
-        self.asoundrc = Asoundrc(cleep_filesystem)
-        self.drivers = drivers.get_drivers(Driver.DRIVER_AUDIO)
 
     def __command(self, command):
         """
@@ -95,21 +47,28 @@ class Alsa(AdvancedConsole):
         Args:
             command (string): command to execute
 
-        Return:
+        Returns:
             dict: dict of outputs::
+
                 {
-                    card name: {
-                        card name (string),
-                        card id (int),
-                        device id (id)
-                    }
+                    card id (int): {
+                        name (string): card name
+                        devices (dict): {
+                            deviceid (int): {
+                                name (string): device name
+                                cardid (int): card id
+                                deviceid (int): device id
+                            },
+                            ...
+                        }
+                    },
+                    ...
                 }
+
         """
         results = self.find(command, r'^(?:card (\d)):.*\[(.*)\].*(?:device (\d)).*\[(.*)\].*$')
         entries = {}
         for group, groups in results:
-            #filter None values
-            #groups = filter(None, groups)
             self.logger.debug('groups (%d): %s' % (len(groups), groups))
 
             if len(groups)==4:
@@ -134,51 +93,72 @@ class Alsa(AdvancedConsole):
                     name = groups[1].strip()
 
                 #store entry
-                entries[name] = {
-                    'name': name,
-                    'cardid': card_id,
-                    'deviceid': device_id
+                if card_id not in entries:
+                    entries[card_id] = {
+                        u'name': name,
+                        u'devices': {}
+                    }
+                entries[card_id][u'devices'][device_id] = {
+                    u'deviceid': device_id,
+                    u'cardid': card_id,
+                    u'name': name
                 }
 
         return entries
+
+    def get_selected_device(self):
+        """
+        Return selected device info
+
+        Returns:
+            dict: selected device info or None if nothing found::
+
+                {
+                    cardname (string): caard name
+                    cardid (int): card id
+                    deviceid (int): device id
+                }
+
+        """
+        #get selected device name
+        cmd = u'amixer info | grep "Card default"'
+        resp = self.command(cmd)
+        if resp[u'error'] or resp[u'killed']:
+            return None
+        try:
+            selected_device_name = resp[u'stdout'][0].split('/')[1].replace('\'','')
+        except:
+            self.logger.exception(u'Error parsing amixer command result:')
+            selected_device_name = None
+
+        #get selected device info
+        for _, device in self.get_playback_devices().items():
+            if device[u'name']==selected_device_name:
+                return device
+
+        return None
 
     def get_playback_devices(self):
         """
         Return list of playback devices
 
-        Return:
+        Returns:
             dict: dict of outputs::
                 {
                     cardname: {
                         cardname (string),
                         cardid (int),
-                        deviceid (id),
-                        type (int): see OUTPUT_TYPE_XXX
-                        supported (bool): supported by raspiot flag
+                        deviceid (id)
                     }
                 }
         """
-        entries = self.__command(u'/usr/bin/aplay --list-devices')
-
-        #append card type and supported flag
-        for name in entries.keys():
-            if name in self.drivers:
-                #supported device
-                entries[name][u'supported'] = True
-                entries[name][u'output_type'] = self.drivers[name].type
-    
-            else:
-                #unsupported devices
-                entries[name][u'supported'] = False
-                entries[name][u'output_type'] = AudioDriver.OUTPUT_TYPE_UNKNOWN
-                
-        return entries
+        return self.__command(u'/usr/bin/aplay --list-devices')
 
     def get_capture_devices(self):
         """
         Return list of capture devices
 
-        Return:
+        Returns:
             dict: dict of outputs::
                 {
                     cardname: {
@@ -197,7 +177,7 @@ class Alsa(AdvancedConsole):
         Args:
             command (string): amixer command. No check is performed to make sure it is a amixer command
 
-        Return:
+        Returns:
             dict:
         """
         self.logger.debug('Amixer command: %s' % command)
@@ -220,108 +200,91 @@ class Alsa(AdvancedConsole):
 
         return entries
 
-    def __get_current_audio_driver(self):
+    def __amixer_control_command(self, command):
         """
-        Return driver of current audio device
+        Execute amixer control command and return formatted outputs
 
-        Return:
-            dict: current profile (see AudioDriver class) or None if device is not supported
-        """
-        #TODO handle cache
-        config = self.asoundrc.get_raw_configuration()
-        self.logger.debug('Asoundrc config: %s' % config)
-
-        #check config content
-        if len(config)==0:
-            #no config, stop here
-            return {}
-        
-        #we only need pcm data (not ctl)
-        if self.asoundrc.PCM_SECTION not in config.keys():
-            self.logger.error('Invalid asoundrc file, %s section is mandatory: %s' % (self.asoundrc.PCM_SECTION, config))
-            raise Exception('Internal error')
-        config = config[self.asoundrc.PCM_SECTION]
-        self.logger.debug('Config: %s' % config)
-
-        #check if device is supported
-        card_name = self.__get_card_name(config[u'cardid'])
-        self.logger.debug('card_name: %s' % card_name)
-        if card_name not in self.drivers:
-            self.logger.error(u'Unable to get volumes: unsupported sound device (card name=%s)' % card_name)
-            return None
-
-        #get profile
-        profile = self.drivers[card_name]
-        self.logger.debug('Found profile: %s' % profile)
-
-        return profile
-
-    def __get_card_name(self, card_id):
-        """
-        Return card name
-       
         Args:
-            card_id (int): card identifier
-       
-        Return:
-            string or None
-        """
-        #search for device id
-        playback_devices = self.get_playback_devices()
-        for device_name in playback_devices.keys():
-            if playback_devices[device_name][u'cardid']==card_id:
-                return device_name
-       
-        return None
+            command (string): amixer command. No check is performed to make sure it is a amixer command
 
-    def __get_or_set_volume(self, driver, get_key, volume=None):
+        Returns:
+            dict:
+        """
+        self.logger.debug('Amixer control command: %s' % command)
+        results = self.find(command, r'(?:(iface)=(.*),)|(?:(name)=\'(.*)\')|(?:(min)=(\d))|(?:(max)=(\d))|(?:(step)=(\d))|(?:: (values)=(.*))')
+
+        values = {}
+        for group, groups in results:
+            #filter None values
+            groups = filter(None, groups)
+            self.logger.debug(groups)
+
+            values[groups[0]] = groups[1]
+
+        return values
+
+    def amixer_control(self, command, numid, value=None):
+        """
+        Execute amixer control
+
+        Args:
+            command (string): Alsa.CGET or Alsa.CSET
+            numid (int): amixer numid
+            value (string): value to use with CSET
+
+        Raises:
+            InvalidParameter: if parameter is invalid
+            MissingParameter: if parameter is missing
+        """
+        #check parameters
+        if command is None:
+            raise MissingParameter(u'Parameter "command" is missing')
+        if command not in (self.CSET, self.CGET):
+            raise InvalidParameter(u'Parameter "command" must be Alsa.CGET or Alsa.CSET')
+        if not isinstance(numid, int):
+            raise InvalidParameter(u'Parameter "numid" must be a string')
+        if command==self.CSET and (value is None or not isinstance(value, str) or len(value)==0):
+            raise MissingParameter(u'Parameter "value" is missing')
+
+        cmd = u'/usr/bin/amixer %s numid=%s %s' % (command, numid, value if value is not None else '')
+        self.logger.debug(u'amixer command: "%s"' % cmd)
+
+        return self.__amixer_control_command(cmd)
+
+    def __get_or_set_volume(self, control, pattern, volume=None):
         """
         Get infos from amixer command, parse results and get volume value
         With volume specified set command is executed, otherwise it is get command
 
         Args:
-            driver (Driver): current device driver
-            get_key (string): GET_CAPTURE or GET_PLAYBACK
+            control (string): amixer control to find
+            pattern (string): pattern to use to extract float value from amixer result
             volume (int): volume percentage or None
 
-        Return:
+        Returns:
             int: volume value or None if error occured
         """
         #check parameters
-        if driver is None:
-            #invalid driver specified, can't perform action
-            self.logger.error(u'Invalid driver specified (driver: %s)' % driver)
-            return 0
-
-        #get control
-        control = getattr(driver, get_key)
-
-        #if no control, nothing else to do
-        if control is None:
-            return None
+        if control is None or len(control)==0:
+            raise MissingParameter(u'Parameter "control" is missing')
+        if pattern is None or len(pattern)==0:
+            raise MissingParameter(u'Parameter "pattern" is missing')
 
         #execute command
         if volume is None:
             results = self.__amixer_command(u'/usr/bin/amixer get "%s"' % control)
         else:
             results = self.__amixer_command(u'/usr/bin/amixer set "%s" %s%%' % (control, volume))
-        self.logger.debug(results)
+        self.logger.debug('Amixer command results: %s' % results)
         if len(results)==0 or control not in results.keys():
             self.logger.warning(u'Unable to get volume: no control "%s" found in results, maybe device does not support it' % control)
             return None
 
         #parse result to get volume value
-        (key, pattern) = getattr(driver, u'%s_data' % get_key)
-        if key is None:
-            self.logger.debug(u'No pattern specified for %s' % (get_key))
-            return None
-        elif key not in results[control].keys():
-            self.logger.error(u'Unable to get volume: no key "%s" in results: %s' % (key, results))
-            return None
-        sub_results = self.find_in_string(results[control][key], pattern, re.UNICODE)
+        sub_results = self.find_in_string(results[control][pattern[0]], pattern[1], re.UNICODE)
         self.logger.debug(u'sub_results=%s' % sub_results)
         if len(sub_results)!=1 or len(sub_results[0])!=2 or len(sub_results[0][1])!=1:
-            self.logger.error(u'Unable to get volume: pattern "%s" seems no valid for string "%s"' % (pattern, results[control][key]))
+            self.logger.error(u'Unable to get volume: pattern "%s" seems no valid for string "%s"' % (pattern[1], results[control][pattern[0]]))
 
         #cast value to int
         volume = None
@@ -333,70 +296,54 @@ class Alsa(AdvancedConsole):
 
         return volume
 
-    def get_volumes(self):
+    def get_volume(self, control, pattern):
         """
-        Return current volume on default playback device
-
-        Return:
-            dict: volumes percentages::
-                {
-                    playback (int or None),
-                    capture (int or None)
-                }
-        """
-        #get current driver
-        driver = self.__get_current_audio_driver()
-        if driver is None:
-            raise CommandError(u'Configured sound device is not supported')
-
-        #get volume values
-        playback_volume = self.__get_or_set_volume(driver, self.__KEY_PLAYBACK)
-        capture_volume = self.__get_or_set_volume(driver, self.__KEY_CAPTURE)
-        self.logger.debug('Volumes: playback=%s capture=%s' % (playback_volume, capture_volume))
-
-        return {
-            u'playback': playback_volume,
-            u'capture': capture_volume,
-        }
-
-    def set_volumes(self, playback=None, capture=None):
-        """
-        Set volumes (individualy or both volumes at the same time)
+        Return current playback volume on current device
 
         Args:
-            playback (int): playback volume to set (if None volume is not updated)
-            capture (int): capture volume to set (if None volume is not updated)
+            control (string): amixer control that contains volume level
+            pattern (string): pattern to use to parse volume level
 
-        Return:
-            dict: new volumes percentages::
-                {
-                    playback (int or None),
-                    capture (int or None)
-                }
+        Returns:
+            float: volume or None if not found
+
+        Raises:
+            MissingParameter: if parameter is missing
+        """
+        #check parameters done in __get_or_set_volume
+
+        volume = self.__get_or_set_volume(control, pattern)
+        self.logger.debug('Volume: %s' % volume)
+
+        return volume
+
+    def set_volume(self, control, pattern, volume):
+        """
+        Set volume
+
+        Args:
+            control (string): amixer control that contains volume level
+            pattern (string): pattern to use to parse volume level
+            volume (float): volume level to set (in percentage)
+
+        Returns:
+            float: new volume or None if failed
+
+        Raises:
+            InvalidParameter: if parameter is invalid
+            MissingParameter: if parameter is missing
         """
         #check parameters
-        if playback is not None and (playback<0 or playback>100):
-            raise InvalidParameter(u'Playback volume value must be 0..100')
-        if capture is not None and (capture<0 or capture>100):
-            raise InvalidParameter(u'Cpature volume value must be 0..100')
-
-        #get current driver
-        driver = self.__get_current_audio_driver()
-        if driver is None:
-            raise CommandError(u'Configured sound device is not supported')
+        if volume is None:
+            raise MissingParameter(u'Parameter "volume" is missing')
+        if volume<0.0 or volume>100.0:
+            raise InvalidParameter(u'Parameter "volume" must be 0...100')
 
         #set volume values
-        playback_volume = None
-        capture_volume = None
-        if playback is not None:
-            playback_volume = self.__get_or_set_volume(driver, self.__KEY_PLAYBACK, playback)
-        if capture is not None:
-            capture_volume = self.__get_or_set_volume(driver, self.__KEY_CAPTURE, capture)
+        volume = self.__get_or_set_volume(control, pattern, volume)
+        self.logger.debug(u'New volume: %s' % volume)
 
-        return {
-            u'playback': playback_volume,
-            u'capture': capture_volume,
-        }
+        return volume
 
     def play_sound(self, path, timeout=5.0):
         """
@@ -405,6 +352,12 @@ class Alsa(AdvancedConsole):
         Args:
             path (string): sound file path
             timeout (float): max playing duration. If you want to play longer sound file, increase the timeout
+
+        Returns:
+            bool: True if command succeed
+
+        Raises:
+            InvalidParameter: if parameter is invalid
         """
         #check params
         if not os.path.exists(path):
@@ -428,8 +381,13 @@ class Alsa(AdvancedConsole):
             channels (int): number of channels to record (default 2 for stereo)
             timeout (float): max recording duration
 
-        Return:
+        Returns:
             string: path of recording file (wav format)
+
+        Raises:
+            MissingParameter: if parameter is missing
+            InvalidParameter: if parameter is invalid
+            CommandError: if error occured during command execution
         """
         #check params
         if format is None:
@@ -454,7 +412,7 @@ class Alsa(AdvancedConsole):
         #check errors
         if res[u'error']:
             self.logger.error(u'Error occured during recording %s: %s' % (cmd, res))
-            raise Exception(u'Error during recording')
+            raise CommandError(u'Error during recording')
 
         return out
 
