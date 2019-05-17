3c9b6a01ed95c1624675010649c139a107075b62
Tangb <tanguy.bonneau@gmail.com>
2019-05-17 09:45:24 +0200

    Improve driver install/uninstall threading process and handling filesystem

--- raspiot/libs/drivers/driver.py
+++ raspiot/libs/drivers/driver.py
@@ -3,6 +3,7 @@
 
 import time
 import logging
+from raspiot.libs.internals.task import Task
 
 class Driver():
     """
@@ -22,19 +23,70 @@ class Driver():
             driver_name (string): driver name.
         """
         self.cleep_filesystem = cleep_filesystem
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.type = driver_type
         self.name = driver_name
 
-    def install(self, params=None):
+    def install(self, end_callback, params=None):
         """
-        Install driver. Don't forget to enable writings during driver installation.
+        Install driver
 
         Args:
+            end_callback (function): function called when install complete. Function args: success (bool), message (string)
             params (dict): additionnal parameters if necessary
+        """
+
+        def install(callback, params):
+            message = None
+            success = True
+            self.cleep_filesystem.enable_write(root=True, boot=True)
+            try:
+                self._install(params)
+            except Exception as e:
+                message = str(e)
+                success = False
+            finally:
+                self.cleep_filesystem.disable_write(root=True, boot=True)
+            callback(success, message)
+
+        task = Task(None, install, self.logger, [end_callback, params])
+        task.start()
+
+    def _install(self, params):
+        """
+        Install driver
+
+        Args:
+            params (dict): optionnal parameters
         """
         raise NotImplementedError(u'Function "install" must be implemented in "%s"' % self.__class__.__name__)
 
-    def uninstall(self, params=None):
+    def uninstall(self, end_callback, params=None):
+        """
+        Uninstall driver
+
+        Args:
+            end_callback (function): function called when install complete. Function args: success (bool), message (string)
+            params (dict): additionnal parameters if necessary
+        """
+
+        def uninstall(callback, params):
+            message = None
+            success = True
+            self.cleep_filesystem.enable_write(root=True, boot=True)
+            try:
+                self._uninstall(params)
+            except Exception as e:
+                message = str(e)
+                success = False
+            finally:
+                self.cleep_filesystem.disable_write(root=True, boot=True)
+            callback(success, message)
+
+        task = Task(None, uninstall, self.logger, [end_callback, params])
+        task.start()
+
+    def _uninstall(self, params=None):
         """
         Uninstall driver. Don't forget to enable writings during driver installation.
 
