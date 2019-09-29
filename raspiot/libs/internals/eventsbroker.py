d3f150c157eea9f4cd34dce2f87b89c4e558b3c9
Tangb <tanguy.bonneau@gmail.com>
2019-09-29 12:42:30 +0200

    Fix last issue again

--- raspiot/libs/internals/eventsbroker.py
+++ raspiot/libs/internals/eventsbroker.py
@@ -101,7 +101,7 @@ class EventsBroker():
 
         for root, _, filenames in os.walk(path):
             for filename in filenames:
-                try
+                try:
                     fullpath = os.path.join(root, filename)
                     (event, ext) = os.path.splitext(filename)
                     parts = full_path_split(fullpath)
@@ -117,11 +117,9 @@ class EventsBroker():
 
                 except AttributeError:
                     self.logger.exception(u'Event "%s" has surely invalid name, please refer to coding rules:' % event)
-                    raise Exception(u'Invalid event tried to be loaded')
 
                 except:
                     self.logger.exception(u'Event "%s" wasn\'t imported successfully. Please check event source code.' % event)
-                    raise Exception(u'Invalid event tried to be loaded')
 
     def get_event_instance(self, event_name):
         """
