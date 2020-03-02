343cda1b81b7f82298062650999b70b5e991ebae
Tangb <tanguy.bonneau@gmail.com>
2020-03-02 09:59:51 +0100

    Add profileformatter unit tests, update profileformatter code

--- raspiot/libs/internals/profileformatter.py
+++ raspiot/libs/internals/profileformatter.py
@@ -4,11 +4,12 @@
 from raspiot.utils import InvalidParameter
 from raspiot.libs.internals.rendererprofile import RendererProfile
 
-__all__ = ['Formatter']
+__all__ = ['ProfileFormatter']
 
-class Formatter():
+class ProfileFormatter():
     """
-    Base formatter class
+    Base ProfileFormatter class
+    Used to format profile with specified event parameters
     """
 
     def __init__(self, events_broker, event_name, profile):
@@ -18,14 +19,14 @@ class Formatter():
         Args:
             events_broker (EventsBroker): events broker instance
             event_name (string): event name compatible with formatter
-            output_profile (RendererProfile): Renderer profile instance
+            profile (RendererProfile): Renderer profile instance
         """
         if not isinstance(event_name, str) and not isinstance(event_name, unicode):
-            raise InvalidParameter(u'Invalid event_name specified (awaited string, found %s)' % (type(event_name)))
+            raise InvalidParameter(u'Invalid event_name specified')
         if not issubclass(profile.__class__, RendererProfile):
-            raise InvalidParameter(u'Invalid profile specified. Instance must inherits from RendererProfile.')
+            raise InvalidParameter(u'Invalid profile specified. Instance must inherits from RendererProfile')
 
-        #members
+        # members
         self.events_broker = events_broker
         self.event_name = event_name
         self.profile_name = profile.__class__.__name__
@@ -34,7 +35,16 @@ class Formatter():
     def format(self, event_params):
         """
         Format event
+
+        Args:
+            event_params (list): list of event parameters as received from internal bus
+
+        Returns:
+            Profile instance filled with appropriate values
         """
+        if not isinstance(event_params, list):
+            raise InvalidParameter(u'Parameter "event_params" must be a list')
+
         return self._fill_profile(event_params, self.profile)
     
     def _fill_profile(self, event_params, profile):
@@ -44,6 +54,9 @@ class Formatter():
         Args:
            event_params (dict): event parameters
            profile (Profile): profile instance
+
+        Returns:
+            Profile instance filled with appropriate values
         """
         raise NotImplementedError('_fill_profile method must be implemented in "%s"' % self.__class__.__name__)
 
---
+++ raspiot/tests/libs/internals/test_profileformatter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.append('%s/../../../libs/internals' % os.getcwd())
+from profileformatter import ProfileFormatter
+from raspiot.libs.tests.lib import TestLib
+from raspiot.libs.internals.rendererprofile import RendererProfile
+from raspiot.utils import InvalidParameter
+import unittest
+import logging
+
+class DummyClass():
+    def __init__(self, *args, **kwargs):
+        pass
+
+class DummyProfile(RendererProfile):
+    def __init__(self, *args, **kwargs):
+        RendererProfile()
+
+class ProfileFormatterTests(unittest.TestCase):
+
+    def setUp(self):
+        TestLib()
+        logging.basicConfig(level=logging.FATAL, format=u'%(asctime)s %(name)s:%(lineno)d %(levelname)s : %(message)s')
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        with self.assertRaises(InvalidParameter) as cm1:
+            ProfileFormatter(DummyClass(), 666, DummyProfile())
+        self.assertEqual(cm1.exception.message, 'Invalid event_name specified')
+        with self.assertRaises(InvalidParameter) as cm2:
+            ProfileFormatter(DummyClass(), 'dummy.event', DummyClass())
+        self.assertEqual(cm2.exception.message, 'Invalid profile specified. Instance must inherits from RendererProfile')
+
+    def test_format_invalid_parameters(self):
+        with self.assertRaises(InvalidParameter) as cm:
+            p = ProfileFormatter(DummyClass(), 'dummy.event', DummyProfile())
+            p.format({})
+        self.assertEqual(cm.exception.message, 'Parameter "event_params" must be a list')
+
+    def test_format_no_fill_profile_implemented(self):
+        with self.assertRaises(NotImplementedError) as cm:
+            p = ProfileFormatter(DummyClass(), 'dummy.event', DummyProfile())
+            p.format([])
+        self.assertEqual(cm.exception.message, '_fill_profile method must be implemented in "ProfileFormatter"')
+
+
+if __name__ == '__main__':
+    #coverage run --omit="/usr/local/lib/python2.7/*","test_*" --concurrency=thread test_profileformatter.py; coverage report -m
+    unittest.main()
+
