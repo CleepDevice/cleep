df508c717d326ec7591a58cc9810e1d540702202
Tangb <tanguy.bonneau@gmail.com>
2020-03-11 18:32:51 +0100

    Auto fill __all__ in __init__ libs file

--- raspiot/libs/commands/__init__.py
+++ raspiot/libs/commands/__init__.py
@@ -1,2 +1,7 @@
-__all__ = ['alsa', 'blkid', 'ifconfig', 'ifupdown', 'iwconfig', 'iwgetid', 'iwlist', 'iw', 'lsblk', 'sox', 'udevadm', 'wpacli']
+from os.path import dirname, basename, isfile, join
+import glob
+
+# auto populate __all__
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
--- raspiot/libs/externals/__init__.py
+++ raspiot/libs/externals/__init__.py
@@ -1,2 +1,7 @@
-__all__ = ['snowboy']
+from os.path import dirname, basename, isfile, join
+import glob
+
+# auto populate __all__
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
