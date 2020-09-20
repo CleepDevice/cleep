3b0a213926d2dce0f8532d4a3aad94eb99abb049
Tangb <tanguy.bonneau@gmail.com>
2020-09-20 23:51:03 +0200

    Update cleep installer according to iso changes (pre and post scripts directly included in deb installer)

--- cleep/libs/internals/installcleep.py
+++ cleep/libs/internals/installcleep.py
@@ -29,19 +29,16 @@ class ForcedException(Exception):
 class InstallCleep(threading.Thread):
     """
     Install cleep update
-    This class download latest archive from cleep repository, executes prescript, installs deb package
-    and ends by post script install
+    This class download latest package from cleep repository, dry run deb install and finally
+    install deb package.
     """
     STATUS_IDLE = 0
     STATUS_UPDATING = 1
     STATUS_UPDATED = 2
     STATUS_ERROR_INTERNAL = 3
     STATUS_ERROR_DOWNLOAD_CHECKSUM = 4
-    STATUS_ERROR_DOWNLOAD_ARCHIVE = 5
-    STATUS_ERROR_EXTRACT = 6
-    STATUS_ERROR_PREINST = 7
-    STATUS_ERROR_DEB = 8
-    STATUS_ERROR_POSTINST = 9
+    STATUS_ERROR_DOWNLOAD_PACKAGE = 5
+    STATUS_ERROR_DEB = 6
 
     def __init__(self, cleep_filesystem, crash_report):
         """
@@ -55,7 +52,7 @@ class InstallCleep(threading.Thread):
 
         # logger   
         self.logger = logging.getLogger(self.__class__.__name__)
-        # self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.DEBUG)
 
         # members
         self.status = self.STATUS_IDLE
@@ -67,17 +64,6 @@ class InstallCleep(threading.Thread):
         self.running = True
         self.__progress = 0
         self.__script_running = True
-        self.__pre_script_execution = False
-        self.__pre_script_status = {
-            u'stdout': [],
-            u'stderr':[],
-            u'returncode': None
-        }
-        self.__post_script_status = {
-            u'stdout': [],
-            u'stderr':[],
-            u'returncode': None
-        }
         self.__deb_status = {
             u'status': InstallDeb.STATUS_IDLE,
             u'stdout': [],
@@ -85,17 +71,17 @@ class InstallCleep(threading.Thread):
         }
         self.__last_download_percent = None
 
-    def install(self, url_archive, url_checksum, callback):
+    def install(self, url_package, url_checksum, callback):
         """
         Launch installation.
         The process is non blocking. Follow install process status using get_status.
 
         Args:
-            url_archive (string): url of cleepos archive
+            url_package (string): url of Cleep deb package
             url_checksum (string): url of checksum
             callback (function): status callback
         """
-        self.url_archive = url_archive
+        self.url_package = url_package
         self.url_checksum = url_checksum
         self.callback = callback
         self.__reset()
@@ -111,8 +97,6 @@ class InstallCleep(threading.Thread):
                 {
                     progress (int): progress percentage
                     status (int): see STATUS_XXX for available codes
-                    prescript (dict): preinst status (returncode, stdout, stderr)
-                    postscript (dict): postinst status (returncode, stdout, stderr)
                     deb (dict): deb status (status, returncode, stdout, stderr)
                 }
 
@@ -120,8 +104,6 @@ class InstallCleep(threading.Thread):
         return {
             u'progress': self.__progress,
             u'status': self.status,
-            u'prescript': self.__pre_script_status,
-            u'postscript': self.__post_script_status,
             u'deb': self.__deb_status
         }
 
@@ -133,87 +115,6 @@ class InstallCleep(threading.Thread):
         if not percent % 10 and percent!=self.__last_download_percent: # pragma: no cover
             self.__last_download_percent = percent
             self.logger.debug('Download callback: %s %s %s' % (status, filesize, percent))
-
-    def __script_callback(self, stdout, stderr):
-        """
-        Get stdout/stderr from script execution
-
-        Args:
-            stdout (string): stdout line
-            stderr (string): stderr line
-        """
-        if self.__pre_script_execution:
-            if stdout:
-                self.__pre_script_status[u'stdout'].append(stdout)
-            if stderr:
-                self.__pre_script_status['stderr'].append(stderr)
-
-        else:
-            if stdout:
-                self.__post_script_status[u'stdout'].append(stdout)
-            if stderr:
-                self.__post_script_status[u'stderr'].append(stderr)
-
-    def __script_terminated_callback(self, return_code, killed):
-        """
-        Get infos when script is terminated
-
-        Note:
-            see http://www.tldp.org/LDP/abs/html/exitcodes.html for return codes
-
-        Args:
-            return_code (int): script return code
-            killed (bool): True if script killed, False otherwise
-        """
-        self.logger.debug(u'Script execution terminated with returncode=%s and killed=%s' % (return_code, killed))
-        if killed: # pragma: no cover
-            if self.__pre_script_execution:
-                self.__pre_script_status[u'returncode'] = 130
-            else:
-                self.__post_script_status[u'returncode'] = 130
-        else:
-            if self.__pre_script_execution:
-                self.__pre_script_status[u'returncode'] = return_code
-            else:
-                self.__post_script_status[u'returncode'] = return_code
-
-        # script execution terminated
-        self.__script_running = False
-
-    def __execute_script(self, path):
-        """
-        Execute specified script
-        This method is blocking !
-
-        Args:
-            path (string): script path
-
-        Returns:
-            bool: True if script execution succeed
-        """
-        # init
-        out = False
-
-        # exec
-        os.chmod(path, stat.S_IEXEC)
-        console = EndlessConsole(path, self.__script_callback, self.__script_terminated_callback)
-
-        # launch script execution
-        console.start()
-
-        # monitor end of script execution
-        self.__script_running = True
-        while self.__script_running:
-            #pause
-            time.sleep(0.25)
-
-        # check script result
-        if self.__pre_script_execution and self.__pre_script_status[u'returncode']==0:
-            out = True
-        elif self.__post_script_status[u'returncode']==0:
-            out = True
-
-        return out
 
     def __download_checksum(self, download, url_checksum):
         """
@@ -235,7 +136,7 @@ class InstallCleep(threading.Thread):
             self.logger.debug(u'Checksum file content: %s' % checksum_content)
 
             checksum = None if checksum_content is None else checksum_content.split()[0]
-            self.logger.debug(u'Cleep archive checksum: %s' % checksum)
+            self.logger.debug(u'Cleep package checksum: %s' % checksum)
 
         except:
             self.logger.exception(u'Exception occured during checksum file download "%s":' % url_checksum)
@@ -244,149 +145,50 @@ class InstallCleep(threading.Thread):
 
         return checksum
 
-    def __download_archive(self, download, url_archive, checksum):
+    def __download_package(self, download, url_package, checksum):
         """
-        Download cleep archive file
+        Download cleep packagee file
 
         Args:
             download (Download): Download instance
-            url_archuive (string): archive file url
+            url_package (string): deb package url
             checksum (string): checksum to use to validate download
 
         Returns:
-            string: downloaded archive path or None if error occured
+            string: downloaded package path or None if error occured
         """
-        archive_path = None
+        package_path = None
 
         try:
-            self.logger.debug(u'Download file "%s"' % self.url_archive)
-            download_status, archive_path = download.download_file(url_archive, check_sha256=checksum)
+            self.logger.debug(u'Download file "%s"' % self.url_package)
+            download_status, package_path = download.download_file(url_package, check_sha256=checksum)
 
-            self.logger.debug(u'Download terminated with status "%s" and archive path "%s"' % (download_status, archive_path))
-            if archive_path is None:
+            self.logger.debug(u'Download terminated with status "%s" and package path "%s"' % (download_status, package_path))
+            if package_path is None:
                 if download_status==Download.STATUS_ERROR:
-                    error = u'Error during "%s" download: internal error' % self.url_archive
+                    error = u'Error during "%s" download: internal error' % self.url_package
                 elif download_status==Download.STATUS_ERROR_INVALIDSIZE:
-                    error = u'Error during "%s" download: invalid filesize' % self.url_archive
+                    error = u'Error during "%s" download: invalid filesize' % self.url_package
                 elif download_status==Download.STATUS_ERROR_BADCHECKSUM:
-                    error = u'Error during "%s" download: invalid checksum' % self.url_archive
+                    error = u'Error during "%s" download: invalid checksum' % self.url_package
                 else:
-                    error = u'Error during "%s" download: unknown error' % self.url_archive
+                    error = u'Error during "%s" download: unknown error' % self.url_package
                 self.logger.error(error)
-                archive_path = None
+                package_path = None
 
         except:
-            self.logger.exception(u'Exception occured during cleepos archive "%s" download:' % url_archive)
+            self.logger.exception(u'Exception occured during cleepos package "%s" download:' % url_package)
             self.crash_report.report_exception()
-            archive_path = None
+            package_path = None
 
-        return archive_path
+        return package_path
 
-    def __extract_archive(self, archive_path):
-        """
-        Extract specified archive
-
-        Args:
-            archive_path (string): archive to extract
-
-        Returns:
-            string: extract path or None if error occured
-        """
-        self.logger.debug('Extracting archive "%s"' % archive_path)
-        extract_path = None
-
-        try:
-            extract_path = tempfile.mkdtemp()
-            self.logger.debug('Extraction path: "%s"' % extract_path)
-
-            zipfile = ZipFile(archive_path, u'r')
-            zipfile.extractall(extract_path)
-            zipfile.close()
-            self.logger.debug(u'Archive extracted successfully')
-
-        except:
-            self.logger.exception(u'Error decompressing cleep archive "%s" in "%s":' % (archive_path, extract_path))
-            self.crash_report.report_exception()
-            extract_path = None
-
-        return extract_path
-
-    def __execute_preinst_script(self, extract_path):
-        """
-        Execute preinst.sh script
-
-        Args:
-            extract_path (string): archive extract path
-
-        Returns:
-            bool: True if prescript execution succeed
-        """
-        error = False
-
-        self.logger.debug(u'Executing preinst.sh script')
-        try:
-            self.__pre_script_execution = True
-            path = os.path.join(extract_path, u'preinst.sh')
-            if os.path.exists(path):
-                self.__script_running = True
-                if not self.__execute_script(path):
-                    # script failed
-                    self.logger.debug(u'Script preinst.sh execution failed')
-                    self.crash_report.manual_report(u'Cleep update failed: prescript failed', self.__pre_script_status)
-                    error = True
-                else:
-                    self.logger.debug(u'Script preinst.sh execution terminated successfully: %s' % self.__pre_script_status)
-            else:
-                self.logger.debug(u'No preinst.sh script in archive')
-
-        except:
-            self.logger.exception(u'Exception occured during preinst.sh script execution:')
-            self.crash_report.report_exception()
-            error = True
-
-        return not error
-
-    def __execute_postinst_script(self, extract_path):
-        """
-        Execute postinst.sh script
-
-        Args:
-            extract_path (string): archive extract path
-
-        Returns:
-            bool: True if prescript execution succeed
-        """
-        error = False
-
-        try:
-            self.logger.debug(u'Executing postinst.sh script')
-            self.__pre_script_execution = False
-            path = os.path.join(extract_path, u'postinst.sh')
-            if os.path.exists(path):
-                self.__script_running = True
-                if not self.__execute_script(path):
-                    # script failed
-                    self.logger.debug(u'Script postinst.sh execution failed: %s' % self.__post_script_status)
-                    self.crash_report.manual_report(u'Cleep update failed: postscript failed', self.__post_script_status)
-                    error = True
-                else:
-                    self.logger.debug(u'Script postinst.sh execution terminated successfully: %s' % self.__post_script_status)
-            else:
-                self.logger.debug(u'No postinst.sh script in archive')
-
-        except:
-            self.logger.exception(u'Exception occured during postinst.sh script execution:')
-            self.crash_report.report_exception()
-            error = True
-
-        return not error
-
-    def __install_deb(self, extract_path):
+    def __install_deb(self, package_path):
         """
         Install deb
 
         Args:
-            extract_path (string): archive extraction path
+            package_path (string): deb package path
 
         Returns:
             bool: True if installation succeed, False otherwise
@@ -395,20 +197,19 @@ class InstallCleep(threading.Thread):
 
         try:
             # prepare installer
-            deb_path = os.path.join(extract_path, u'cleep.deb')
-            self.logger.debug('Installing "%s" debian package' % deb_path)
+            self.logger.debug('Installing "%s" debian package' % package_path)
             installer = InstallDeb(self.cleep_filesystem, self.crash_report)
 
             # dry run install
-            if not installer.dry_run(deb_path):
-                #dry run failed, report error and quit install
+            if not installer.dry_run(package_path):
+                # dry run failed, report error and quit install
                 self.logger.error(u'Install dry-run failed: %s' % installer.get_status())
                 self.crash_report.manual_report(u'Dry-run cleep install failed', installer.get_status())
-                raise ForcedException()
+                raise ForcedException(0)
 
             # install deb
             self.logger.debug(u'Waiting for end of debian package install...')
-            installer.install(deb_path, blocking=True)
+            installer.install(package_path, blocking=True)
             self.logger.debug(u'Deb package install terminated with status: %s' % installer.get_status())
 
             # check deb install result
@@ -421,7 +222,7 @@ class InstallCleep(threading.Thread):
             error = True
 
         except:
-            #deb install failed
+            # deb install failed
             self.logger.exception(u'Exception occured during deb package install:')
             self.crash_report.report_exception()
             error = True
@@ -440,9 +241,8 @@ class InstallCleep(threading.Thread):
         self.logger.info(u'Start Cleep update')
         self.status = self.STATUS_UPDATING
         error = False
-        archive_path = None
+        package_path = None
         extract_path = None
-
 
         try:
             # send status asap to update frontend
@@ -459,59 +259,28 @@ class InstallCleep(threading.Thread):
 
             # send status
             if self.callback:
-                self.__progress = 16
+                self.__progress = 33
                 self.callback(self.get_status())
 
-            # download cleep archive
-            archive_path = self.__download_archive(download, self.url_archive, checksum)
-            if not archive_path:
-                self.status = self.STATUS_ERROR_DOWNLOAD_ARCHIVE
+            # download cleep deb package
+            package_path = self.__download_package(download, self.url_package, checksum)
+            if not package_path:
+                self.status = self.STATUS_ERROR_DOWNLOAD_PACKAGE
                 raise ForcedException(1)
 
             # send status
             if self.callback:
-                self.__progress = 32
+                self.__progress = 66
                 self.callback(self.get_status())
 
-            # extract archive
-            extract_path = self.__extract_archive(archive_path)
-            if not extract_path:
-                self.status = self.STATUS_ERROR_EXTRACT
+            # install deb package
+            if not self.__install_deb(package_path):
+                self.status = self.STATUS_ERROR_DEB
                 raise ForcedException(2)
 
             # send status
             if self.callback:
-                self.__progress = 48
-                self.callback(self.get_status())
-
-            # pre update script
-            if not self.__execute_preinst_script(extract_path):
-                self.status = self.STATUS_ERROR_PREINST
-                raise ForcedException(3)
-
-            # send status
-            if self.callback:
-                self.__progress = 64
-                self.callback(self.get_status())
-
-            # install deb package
-            if not self.__install_deb(extract_path):
-                self.status = self.STATUS_ERROR_DEB
-                raise ForcedException(4)
-
-            # send status
-            if self.callback:
-                self.__progress = 80
-                self.callback(self.get_status())
-
-            # post update script
-            if not self.__execute_postinst_script(extract_path):
-                self.status = self.STATUS_ERROR_POSTINST
-                raise ForcedException(5)
-
-            # send status
-            if self.callback:
-                self.__progress = 96
+                self.__progress = 100
                 self.callback(self.get_status())
 
         except ForcedException as e:
@@ -527,8 +296,8 @@ class InstallCleep(threading.Thread):
         finally:
             # clean stuff
             try:
-                if archive_path:
-                    self.cleep_filesystem.rm(archive_path)
+                if package_path:
+                    self.cleep_filesystem.rm(package_path)
                 if extract_path:
                     self.cleep_filesystem.rmdir(extract_path)
             except: # pragma: no cover
