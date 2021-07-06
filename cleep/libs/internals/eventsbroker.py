b53109e530318f1de0ad6a27ce7d776eec89d020
tang <tanguy.bonneau@gmail.com>
2021-07-06 22:46:52 +0200

    Update unit tests for eventsbroker

--- cleep/libs/internals/eventsbroker.py
+++ cleep/libs/internals/eventsbroker.py
@@ -5,23 +5,23 @@ import logging
 import os
 import importlib
 import inspect
-from cleep.exception import MissingParameter, InvalidParameter, CommandError
 from cleep.libs.internals.profileformatter import ProfileFormatter
 from cleep.libs.internals.tools import full_split_path
 
-__all__ = ['EventsBroker']
+__all__ = ["EventsBroker"]
 
-class EventsBroker():
+
+class EventsBroker:
     """
     Events broker
     The goal of this broker is to centralize events to reference all them all in the core.
     It is also used to check event content before posting them and make sure it is compatible with core.
     """
 
-    PYTHON_CLEEP_APPS_IMPORT_PATH = 'cleep.modules.'
-    PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH = 'cleep.events.'
-    MODULES_DIR = '../../modules'
-    CORE_EVENTS_DIR = '../../events'
+    PYTHON_CLEEP_APPS_IMPORT_PATH = "cleep.modules."
+    PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH = "cleep.events."
+    MODULES_DIR = "../../modules"
+    CORE_EVENTS_DIR = "../../events"
 
     def __init__(self, debug_enabled):
         """
@@ -51,11 +51,9 @@ class EventsBroker():
         """
         # set members
         self.bootstrap = bootstrap
-        self.internal_bus = bootstrap['internal_bus']
-        self.formatters_broker = bootstrap['formatters_broker']
-
-        # configure crash report
-        self.crash_report = bootstrap['crash_report']
+        self.internal_bus = bootstrap["internal_bus"]
+        self.formatters_broker = bootstrap["formatters_broker"]
+        self.crash_report = bootstrap["crash_report"]
 
         # load events
         self.__load_events()
@@ -67,7 +65,7 @@ class EventsBroker():
         Returns:
             string: external bus name
         """
-        return self.bootstrap.get('external_bus', None)
+        return self.bootstrap.get("external_bus", None)
 
     def __get_event_class_name(self, filename, module):
         """
@@ -77,7 +75,9 @@ class EventsBroker():
             filename (string): filename (without extension)
             module (module): python module
         """
-        return next((item for item in dir(module) if item.lower()==filename.lower()), None)
+        return next(
+            (item for item in dir(module) if item.lower() == filename.lower()), None
+        )
 
     def __load_events(self):
         """
@@ -86,7 +86,10 @@ class EventsBroker():
         self.__load_events_from_core()
         self.__load_events_from_modules_dir()
 
-        self.logger.debug('Found %d events: %s' % (len(self.events_by_event), self.events_by_event.keys()))
+        self.logger.debug(
+            "Found %d events: %s"
+            % (len(self.events_by_event), self.events_by_event.keys())
+        )
 
     def __save_event(self, class_):
         """
@@ -96,11 +99,11 @@ class EventsBroker():
             class_ (class): event class ready to be instanciated
         """
         self.events_by_event[class_.EVENT_NAME] = {
-            'class': class_,
-            'used': False,
-            'modules': [],
-            'formatters': [],
-            'profiles': [],
+            "class": class_,
+            "used": False,
+            "modules": [],
+            "formatters": [],
+            "profiles": [],
         }
 
     def __load_events_from_core(self):
@@ -108,13 +111,14 @@ class EventsBroker():
         Load existing events from core
         Create an event instance (singleton) for each ones
         """
-        path = os.path.abspath(os.path.join(os.path.dirname(__file__), self.CORE_EVENTS_DIR))
+        path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), self.CORE_EVENTS_DIR)
+        )
         self.logger.debug('Loading events from core dir "%s"' % path)
         if not os.path.exists(path):
-            self.crash_report.report_exception({
-                'message': 'Invalid core events path',
-                'path': path
-            })
+            self.crash_report.report_exception(
+                {"message": "Invalid core events path", "path": path}
+            )
             raise Exception('Invalid core events path "%s"' % path)
 
         for root, _, filenames in os.walk(path):
@@ -123,25 +127,43 @@ class EventsBroker():
                 try:
                     fullpath = os.path.join(root, filename)
                     (event, ext) = os.path.splitext(filename)
-                    parts = full_split_path(fullpath)
-                    if filename.lower().find('event')>=0 and ext=='.py':
-                        self.logger.trace('Loading "%s"' % '%s%s' % (self.PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH, event))
-                        mod_ = importlib.import_module('%s%s' % (self.PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH, event))
+                    if filename.lower().find("event") >= 0 and ext == ".py":
+                        self.logger.trace(
+                            'Loading "%s"'
+                            % "%s%s"
+                            % (self.PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH, event)
+                        )
+                        mod_ = importlib.import_module(
+                            "%s%s" % (self.PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH, event)
+                        )
                         event_class_name = self.__get_event_class_name(event, mod_)
-                        self.logger.trace('Found event class name: %s' % event_class_name)
+                        self.logger.trace(
+                            "Found event class name: %s" % event_class_name
+                        )
                         if event_class_name:
                             class_ = getattr(mod_, event_class_name)
                             self.__save_event(class_)
-                            self.logger.debug('Core event "%s" registered' % event_class_name)
+                            self.logger.debug(
+                                'Core event "%s" registered' % event_class_name
+                            )
                         else:
-                            self.logger.error('Event class must have the same name than the filename')
+                            self.logger.error(
+                                'Event class must have the same name than the filename "%s"'
+                                % fullpath
+                            )
 
-                except AttributeError: # pragma: no cover
-                    self.logger.exception('Event "%s" has surely invalid name, please refer to coding rules:' % event)
+                except AttributeError:  # pragma: no cover
+                    self.logger.exception(
+                        'Event "%s" has surely invalid name, please refer to coding rules:'
+                        % event
+                    )
                     continue
 
-                except:
-                    self.logger.exception('Event "%s" wasn\'t imported successfully. Please check event source code.' % event)
+                except Exception:
+                    self.logger.exception(
+                        'Event "%s" wasn\'t imported successfully. Please check event source code.'
+                        % event
+                    )
                     continue
 
     def __load_events_from_modules_dir(self):
@@ -149,13 +171,14 @@ class EventsBroker():
         Load existing events from modules directory.
         Create an event instance (singleton) for each ones.
         """
-        path = os.path.abspath(os.path.join(os.path.dirname(__file__), self.MODULES_DIR))
+        path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), self.MODULES_DIR)
+        )
         self.logger.debug('Loading events from modules dir "%s"' % path)
         if not os.path.exists(path):
-            self.crash_report.report_exception({
-                'message': 'Invalid modules path',
-                'path': path
-            })
+            self.crash_report.report_exception(
+                {"message": "Invalid modules path", "path": path}
+            )
             raise Exception('Invalid modules path "%s"' % path)
 
         for root, _, filenames in os.walk(path):
@@ -165,24 +188,44 @@ class EventsBroker():
                     fullpath = os.path.join(root, filename)
                     (event, ext) = os.path.splitext(filename)
                     parts = full_split_path(fullpath)
-                    if filename.lower().find('event')>=0 and ext=='.py':
-                        self.logger.debug('Loading "%s"' % '%s%s.%s' % (self.PYTHON_CLEEP_APPS_IMPORT_PATH, parts[-2], event))
-                        mod_ = importlib.import_module('%s%s.%s' % (self.PYTHON_CLEEP_APPS_IMPORT_PATH, parts[-2], event))
+                    if filename.lower().find("event") >= 0 and ext == ".py":
+                        self.logger.debug(
+                            'Loading "%s"'
+                            % "%s%s.%s"
+                            % (self.PYTHON_CLEEP_APPS_IMPORT_PATH, parts[-2], event)
+                        )
+                        mod_ = importlib.import_module(
+                            "%s%s.%s"
+                            % (self.PYTHON_CLEEP_APPS_IMPORT_PATH, parts[-2], event)
+                        )
                         event_class_name = self.__get_event_class_name(event, mod_)
-                        self.logger.trace('Found event class name: %s' % event_class_name)
+                        self.logger.trace(
+                            "Found event class name: %s" % event_class_name
+                        )
                         if event_class_name:
                             class_ = getattr(mod_, event_class_name)
                             self.__save_event(class_)
-                            self.logger.debug('Module event "%s" registered' % event_class_name)
+                            self.logger.debug(
+                                'Module event "%s" registered' % event_class_name
+                            )
                         else:
-                            self.logger.error('Event class must have the same name than the filename')
+                            self.logger.error(
+                                'Event class must have the same name than the filename "%s"'
+                                % fullpath
+                            )
 
-                except AttributeError: # pragma: no cover
-                    self.logger.exception('Event "%s" has surely invalid name, please refer to coding rules:' % event)
+                except AttributeError:  # pragma: no cover
+                    self.logger.exception(
+                        'Event "%s" has surely invalid name, please refer to coding rules:'
+                        % event
+                    )
                     continue
 
-                except:
-                    self.logger.exception('Event "%s" wasn\'t imported successfully. Please check event source code.' % event)
+                except Exception:
+                    self.logger.exception(
+                        'Event "%s" wasn\'t imported successfully. Please check event source code.'
+                        % event
+                    )
                     continue
 
     def get_event_instance(self, event_name):
@@ -202,27 +245,29 @@ class EventsBroker():
         if event_name in self.events_by_event.keys():
             # get module caller
             stack = inspect.stack()
-            caller = stack[1][0].f_locals['self']
+            caller = stack[1][0].f_locals["self"]
             module = None
             formatter = None
             if issubclass(caller.__class__, ProfileFormatter):
                 # formatter registers event
                 formatter = caller.__class__.__name__.lower()
-                self.logger.debug('Formatter %s registers event %s' % (formatter, event_name))
+                self.logger.debug(
+                    "Formatter %s registers event %s" % (formatter, event_name)
+                )
             else:
                 # module registers event
                 module = caller.__class__.__name__.lower()
-                self.logger.debug('Module %s registers event %s' % (module, event_name))
+                self.logger.debug("Module %s registers event %s" % (module, event_name))
 
             # update events by event dict
-            self.events_by_event[event_name]['used'] = True
+            self.events_by_event[event_name]["used"] = True
             if module:
-                self.events_by_event[event_name]['modules'].append(module)
+                self.events_by_event[event_name]["modules"].append(module)
             if formatter:
-                self.events_by_event[event_name]['formatters'].append(formatter)
+                self.events_by_event[event_name]["formatters"].append(formatter)
                 profile_name = caller.profile.__class__.__name__
-                if profile_name not in self.events_by_event[event_name]['profiles']:
-                    self.events_by_event[event_name]['profiles'].append(profile_name)
+                if profile_name not in self.events_by_event[event_name]["profiles"]:
+                    self.events_by_event[event_name]["profiles"].append(profile_name)
 
             # update events by module dict
             if module:
@@ -230,12 +275,14 @@ class EventsBroker():
                     self.events_by_module[module] = []
                 self.events_by_module[module].append(event_name)
 
-            return self.events_by_event[event_name]['class']({
-                'internal_bus': self.internal_bus,
-                'formatters_broker': self.formatters_broker,
-                'get_external_bus_name': self.__get_external_bus_name,
-                'events_broker': self,
-            })
+            return self.events_by_event[event_name]["class"](
+                {
+                    "internal_bus": self.internal_bus,
+                    "formatters_broker": self.formatters_broker,
+                    "get_external_bus_name": self.__get_external_bus_name,
+                    "events_broker": self,
+                }
+            )
 
         raise Exception('Event "%s" does not exist' % event_name)
 
@@ -255,7 +302,11 @@ class EventsBroker():
                 }
 
         """
-        return { k:{'modules':v['modules'], 'profiles':v['profiles']} for k,v in self.events_by_event.items() if v['used']}
+        return {
+            k: {"modules": v["modules"], "profiles": v["profiles"]}
+            for k, v in self.events_by_event.items()
+            if v["used"]
+        }
 
     def get_modules_events(self):
         """
@@ -299,13 +350,21 @@ class EventsBroker():
             renderer_name (string): renderer name
             renderable (bool): True if event is renderable
         """
-        if renderable and event_name in self.events_not_renderable and renderer_name in self.events_not_renderable[event_name]:
+        if (
+            renderable
+            and event_name in self.events_not_renderable
+            and renderer_name in self.events_not_renderable[event_name]
+        ):
             # event is now renderable for renderer, remove entry
             self.events_not_renderable[event_name].remove(renderer_name)
         elif not renderable and event_name not in self.events_not_renderable:
             # event is not renderable, add entry
             self.events_not_renderable[event_name] = [renderer_name]
-        elif not renderable and event_name in self.events_not_renderable and renderer_name not in self.events_not_renderable[event_name]:
+        elif (
+            not renderable
+            and event_name in self.events_not_renderable
+            and renderer_name not in self.events_not_renderable[event_name]
+        ):
             # event is not renderable, append entry
             self.events_not_renderable[event_name].append(renderer_name)
 
@@ -323,5 +382,4 @@ class EventsBroker():
         if event_name not in self.events_not_renderable:
             return True
 
-        return False if renderer_name in self.events_not_renderable[event_name] else True
-
+        return renderer_name not in self.events_not_renderable[event_name]
--- cleep/tests/libs/internals/test_eventsbroker.py
+++ cleep/tests/libs/internals/test_eventsbroker.py
@@ -24,23 +24,23 @@ class DummyFormatter(ProfileFormatter):
     def get_event_instance(self, event_name):
         return self.events_broker.get_event_instance(event_name)
 
-EVENT_CONTENT = u"""from cleep.libs.internals.event import Event
+EVENT_CONTENT = """from cleep.libs.internals.event import Event
 class %(event_class)s(Event):
-    EVENT_NAME = u'%(event_name)s'
+    EVENT_NAME = '%(event_name)s'
     EVENT_PARAMS = []
     def __init__(self, params):
         Event.__init__(self, params)
 """
-EVENT_CONTENT_INVALID_CLASSNAME = u"""from cleep.libs.internals.event import Event
+EVENT_CONTENT_INVALID_CLASSNAME = """from cleep.libs.internals.event import Event
 class DummyEvent(Event):
-    EVENT_NAME = u'%(event_name)s'
+    EVENT_NAME = '%(event_name)s'
     EVENT_PARAMS = []
     def __init__(self, params):
         Event.__init__(self, params)
 """
-EVENT_CONTENT_SYNTAX_ERROR = u"""from cleep.libs.internals.event import Event
+EVENT_CONTENT_SYNTAX_ERROR = """from cleep.libs.internals.event import Event
 class %(event_class)s(Event):
-    EVENT_NAME = u'%(event_name)s'
+    EVENT_NAME = '%(event_name)s'
     EVENT_PARAMS = []
     def __init__(self, params):
         Event.__init__(self, params)
@@ -53,12 +53,13 @@ class %(event_class)s(Event):
 class EventsBrokerTests(unittest.TestCase):
 
     MODULES_DIR = '/tmp/test_modules'
+    CORE_EVENTS_DIR = '/tmp/test_core'
     EVENT_NAME1 = 'event1'
     EVENT_NAME2 = 'event2'
 
     def setUp(self):
         TestLib()
-        logging.basicConfig(level=logging.FATAL, format=u'%(asctime)s %(name)s:%(lineno)d %(levelname)s : %(message)s')
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s:%(lineno)d %(levelname)s : %(message)s')
 
         self.crash_report = Mock()
         self.internal_bus = Mock()
@@ -75,14 +76,18 @@ class EventsBrokerTests(unittest.TestCase):
     def tearDown(self):
         if os.path.exists(self.MODULES_DIR):
             shutil.rmtree(self.MODULES_DIR)
+        if os.path.exists(self.CORE_EVENTS_DIR):
+            shutil.rmtree(self.CORE_EVENTS_DIR)
 
         # unload imported events
         if 'app1.app1EventTest' in sys.modules:  
             del sys.modules['app1.app1EventTest']
         if 'app2.app2EventTest' in sys.modules:  
             del sys.modules['app2.app2EventTest']
+        if 'coreEventTest' in sys.modules:
+            del sys.modules['coreEventTest']
 
-    def _init_context(self, event_content=EVENT_CONTENT, add_apps=True):
+    def _init_context(self, event_content=EVENT_CONTENT, add_apps=True, add_core=False):
         # build fake apps tree
         os.mkdir(self.MODULES_DIR)
         if add_apps:
@@ -91,9 +96,9 @@ class EventsBrokerTests(unittest.TestCase):
                 event_class = '%sEventTest' % app_name
                 os.mkdir(app_dir)
                 with io.open(os.path.join(app_dir, '__init__.py'), 'w') as fd:
-                    fd.write(u'')
+                    fd.write('')
                 with io.open(os.path.join(app_dir, '%s.py' % app_name), 'w') as fd:
-                    fd.write(u'')
+                    fd.write('')
                 with io.open(os.path.join(app_dir, '%s.py' % event_class), 'w') as fd:
                     fd.write(event_content % {
                         'event_class': event_class,
@@ -102,10 +107,27 @@ class EventsBrokerTests(unittest.TestCase):
         # inject fake apps root dir to import path
         sys.path.append(os.path.join(os.getcwd(), self.MODULES_DIR))
 
+        # core events
+        os.mkdir(self.CORE_EVENTS_DIR)
+        if add_core:
+            app_dir = self.CORE_EVENTS_DIR
+            with io.open(os.path.join(app_dir, '__init__.py'), 'w') as fd:
+                fd.write('')
+            event_class = 'coreEventTest'
+            with io.open(os.path.join(app_dir, '%s.py' % event_class), 'w') as fd:
+                fd.write(event_content % {
+                    'event_class': event_class,
+                    'event_name': 'core.event.test'
+                })
+
+        # inject fake apps root dir to import path
+        sys.path.append(os.path.join(os.getcwd(), self.CORE_EVENTS_DIR))
+
         # overwrite module paths
-        # self.e.MODULES_DIR = '../../tests/libs/internals/%s' % self.MODULES_DIR
         self.e.MODULES_DIR = self.MODULES_DIR
-        self.e.PYTHON_CLEEP_IMPORT_PATH = ''
+        self.e.CORE_EVENTS_DIR = self.CORE_EVENTS_DIR
+        self.e.PYTHON_CLEEP_APPS_IMPORT_PATH = ''
+        self.e.PYTHON_CLEEP_CORE_EVENTS_IMPORT_PATH = ''
 
     def test_get_external_bus_name(self):
         self._init_context()
@@ -141,17 +163,57 @@ class EventsBrokerTests(unittest.TestCase):
             self.e.configure(self.bootstrap)
         self.assertTrue(str(cm.exception).startswith('Invalid modules path'))
 
-    def test_load_events_invalid_classname(self):
-        self._init_context(event_content=EVENT_CONTENT_INVALID_CLASSNAME)
+    def test_load_events_from_core(self):
+        self._init_context(add_apps=False, add_core=True)
         self.e.configure(self.bootstrap)
-        
-        self.assertEqual(len(self.e.events_by_event), 0)
 
-    def test_load_events_invalid_syntax(self):
-        self._init_context(event_content=EVENT_CONTENT_SYNTAX_ERROR)
+        logging.debug('Events: %s' % self.e.events_by_event)
+
+        self.assertEqual(list(self.e.events_by_event.keys())[0], 'core.event.test')
+
+    def test_load_events_from_core_crash_report(self):
+        self._init_context(add_apps=False, add_core=True)
+        self.e.CORE_EVENTS_DIR = '/tmp/dummy'
+
+        with self.assertRaises(Exception) as cm:
+            self.e.configure(self.bootstrap)
+        self.assertEqual(str(cm.exception), 'Invalid core events path "/tmp/dummy"')
+        self.crash_report.report_exception.assert_called_with({
+            'message': 'Invalid core events path',
+            'path': '/tmp/dummy'
+        })
+
+    def test_load_events_from_core_invalid_classname(self):
+        self._init_context(add_apps=False, add_core=True, event_content=EVENT_CONTENT_INVALID_CLASSNAME)
+        self.e.logger = Mock()
+        self.e.configure(self.bootstrap)
+
+        self.assertEqual(len(self.e.events_by_event), 0)
+        self.e.logger.error.assert_called_with('Event class must have the same name than the filename "/tmp/test_core/coreEventTest.py"')
+
+    def test_load_events_from_core_invalid_syntax(self):
+        self._init_context(add_apps=False, add_core=True, event_content=EVENT_CONTENT_SYNTAX_ERROR)
+        self.e.logger = Mock()
+        self.e.configure(self.bootstrap)
+
+        self.assertEqual(len(self.e.events_by_event), 0)
+        self.e.logger.exception.assert_called_with('Event "coreEventTest" wasn\'t imported successfully. Please check event source code.')
+
+    def test_load_events_from_apps_invalid_classname(self):
+        self._init_context(event_content=EVENT_CONTENT_INVALID_CLASSNAME)
+        self.e.logger = Mock()
         self.e.configure(self.bootstrap)
         
         self.assertEqual(len(self.e.events_by_event), 0)
+        self.e.logger.error.assert_called_with('Event class must have the same name than the filename "/tmp/test_modules/app2/app2EventTest.py"')
+
+    def test_load_events_from_apps_invalid_syntax(self):
+        self._init_context(event_content=EVENT_CONTENT_SYNTAX_ERROR)
+        self.e.logger = Mock()
+        self.e.configure(self.bootstrap)
+        
+        self.assertEqual(len(self.e.events_by_event), 0)
+        self.e.logger.exception.assert_called_with('Event "app2EventTest" wasn\'t imported successfully. Please check event source code.')
 
     def test_get_event_instance(self):
         self._init_context()
@@ -220,9 +282,39 @@ class EventsBrokerTests(unittest.TestCase):
             self.e.get_module_events('dummy')
         self.assertEqual(str(cm.exception), 'Module name "dummy" is not referenced in Cleep')
 
+    def test_set_event_renderable(self):
+        self._init_context()
+        self.e.configure(self.bootstrap)
+        
+        self.e.set_event_renderable('test.event.app1', 'renderer', False)
+        logging.debug('Events not renderable: %s' % self.e.events_not_renderable)
+        self.assertTrue('test.event.app1' in self.e.events_not_renderable)
+        self.assertTrue('renderer' in self.e.events_not_renderable['test.event.app1'])
+
+        self.e.set_event_renderable('test.event.app1', 'otherrenderer', False)
+        logging.debug('Events not renderable: %s' % self.e.events_not_renderable)
+        self.assertTrue('otherrenderer' in self.e.events_not_renderable['test.event.app1'])
+
+        self.e.set_event_renderable('test.event.app1', 'renderer', True)
+        logging.debug('Events not renderable: %s' % self.e.events_not_renderable)
+        self.assertFalse('renderer' in self.e.events_not_renderable['test.event.app1'])
+        
+        self.e.set_event_renderable('test.event.app1', 'otherrenderer', True)
+        logging.debug('Events not renderable: %s' % self.e.events_not_renderable)
+        self.assertFalse('otherrenderer' in self.e.events_not_renderable['test.event.app1'])
+
+    def test_is_event_renderable(self):
+        self._init_context()
+        self.e.configure(self.bootstrap)
+        logging.debug('Events by event: %s' % self.e.events_by_event)
+        logging.debug('Events not renderable: %s' % self.e.events_not_renderable)
+
+        self.assertTrue(self.e.is_event_renderable('test.event.app1', 'renderer'))
+        self.e.set_event_renderable('test.event.app1', 'renderer', False)
+        self.assertFalse(self.e.is_event_renderable('test.event.app1', 'renderer'))
 
 
 if __name__ == '__main__':
-    #coverage run --omit="/usr/local/lib/python*/*","*test_*.py" --concurrency=thread test_eventsbroker.py; coverage report -m -i
+    # coverage run --omit="*/lib/python*/*","*test_*.py" --concurrency=thread test_eventsbroker.py; coverage report -m -i
     unittest.main()
 
