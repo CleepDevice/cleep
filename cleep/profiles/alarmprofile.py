28e319a79db1523dc8cdb9a43e3a345041eea70f
tang <tanguy.bonneau@gmail.com>
2021-12-21 21:41:27 +0100

    Add alarm profile

---
+++ cleep/profiles/alarmprofile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cleep.libs.internals.rendererprofile import RendererProfile
+
+class AlarmProfile(RendererProfile):
+    """
+    Alarm profile.
+
+    Used to trigger an alert for renderer that support it
+    """
+
+    STATUS_UNKNOWN = 'unknown'
+    STATUS_SCHEDULED = 'scheduled'
+    STATUS_TRIGGERED = 'triggered'
+    STATUS_STOPPED = 'stopped'
+    STATUS_SNOOZED = 'snoozed'
+
+    def __init__(self):
+        RendererProfile.__init__(self)
+        self.hour = None
+        self.minute = None
+        self.duration = None
+        self.status = self.STATUS_UNKNOWN
+
