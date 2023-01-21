b22252c73fa4d130da16c2df946b93d8b9c66ae6
tang <tanguy.bonneau@gmail.com>
2023-01-21 18:13:45 +0100

    Fix blkid lib unit tests

--- cleep/tests/libs/commands/test_blkid.py
+++ cleep/tests/libs/commands/test_blkid.py
@@ -3,7 +3,8 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__)).replace('tests/', ''))
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)).replace("tests/", ""))
 from blkid import Blkid
 from cleep.libs.internals.cleepfilesystem import CleepFilesystem
 from cleep.exception import MissingParameter, InvalidParameter, CommandError
@@ -12,47 +13,57 @@ import unittest
 import logging
 from pprint import pformat
 from cleep.libs.tests.common import get_log_level
+from mock import Mock
 
 LOG_LEVEL = get_log_level()
+CONTENT = [
+    "/dev/mmcblk0p1: LABEL_FATBOOT=\"boot\" LABEL=\"boot\" UUID=\"EBBA-157F\" TYPE=\"vfat\" PARTUUID=\"b294e190-01\"",
+    "/dev/mmcblk0p2: LABEL=\"rootfs\" UUID=\"b3ce35cd-ade9-4755-a4bb-1571e37fc1b9\" TYPE=\"ext4\" PARTUUID=\"b294e190-02\"",
+    "/dev/mmcblk0: PTUUID=\"b294e190\" PTTYPE=\"dos\"",
+]
 
 class BlkidTests(unittest.TestCase):
-
     def setUp(self):
         TestLib()
-        logging.basicConfig(level=LOG_LEVEL, format=u'%(asctime)s %(name)s %(levelname)s : %(message)s')
+        logging.basicConfig(
+            level=LOG_LEVEL, format=u"%(asctime)s %(name)s %(levelname)s : %(message)s"
+        )
         self.b = Blkid()
+        self.b.command = Mock(return_value={"killed": False, "error": False, "stdout": CONTENT})
 
     def tearDown(self):
         pass
 
     def test_get_devices(self):
         devices = self.b.get_devices()
-        logging.debug('Devices: %s' % devices)
-        self.assertEqual(len(devices), 2, 'Blkid should find 2 mountpoints')
+        logging.debug("Devices: %s" % devices)
+        self.assertEqual(len(devices), 2, "Blkid should find 2 mountpoints")
         for device, _ in devices.items():
-            self.assertTrue(device.find('/dev')!=-1, 'Device should contains /dev/xxx')
+            self.assertTrue(
+                device.find("/dev") != -1, "Device should contains /dev/xxx"
+            )
 
     def test_get_device_by_uuid(self):
         devices = self.b.get_devices()
-        logging.debug('Devices: %s' % self.b.devices)
+        logging.debug("Devices: %s" % self.b.devices)
         device = devices[list(devices.keys())[0]]
-        logging.debug('Device: %s' % device)
-        result = self.b.get_device_by_uuid(device[u'uuid'])
-        self.assertEqual(result['device'], device['device'])
+        logging.debug("Device: %s" % device)
+        result = self.b.get_device_by_uuid(device[u"uuid"])
+        self.assertEqual(result["device"], device["device"])
 
     def test_get_device_by_uuid_invalid_uuid(self):
-        self.assertEqual(self.b.get_device_by_uuid('uuid'), None)
+        self.assertEqual(self.b.get_device_by_uuid("uuid"), None)
 
     def test_get_device_by_partuuid(self):
         devices = self.b.get_devices()
-        logging.debug('Devices: %s' % self.b.devices)
+        logging.debug("Devices: %s" % self.b.devices)
         device = devices[list(devices.keys())[0]]
-        logging.debug('Device: %s' % device)
-        result = self.b.get_device_by_partuuid(device[u'partuuid'])
-        self.assertEqual(result['device'], device['device'])
+        logging.debug("Device: %s" % device)
+        result = self.b.get_device_by_partuuid(device[u"partuuid"])
+        self.assertEqual(result["device"], device["device"])
 
     def test_get_device_by_partuuid_invalid_partuuid(self):
-        self.assertEqual(self.b.get_device_by_partuuid('uuid'), None)
+        self.assertEqual(self.b.get_device_by_partuuid("uuid"), None)
 
     def test_get_device(self):
         devices = self.b.get_devices()
@@ -61,8 +72,9 @@ class BlkidTests(unittest.TestCase):
         self.assertEqual(self.b.get_device(mountpoint), uuid)
 
     def test_get_device_invalid_device(self):
-        self.assertEqual(self.b.get_device('mountpoint'), None)
+        self.assertEqual(self.b.get_device("mountpoint"), None)
 
-if __name__ == '__main__':
-    # coverage run --omit="*lib/python*/*","*test_*.py" --concurrency=thread test_blkid.py; coveverage report -m -i
+
+if __name__ == "__main__":
+    # coverage run --omit="*lib/python*/*","*test_*.py" --concurrency=thread test_blkid.py; coverage report -m -i
     unittest.main()
