f315f7486138cb531358156d2bbbe9eecd12dbd1
tang <tanguy.bonneau@gmail.com>
2023-01-21 17:20:11 +0100

    Fix fstab lib unit tests

--- cleep/tests/libs/configs/test_fstab.py
+++ cleep/tests/libs/configs/test_fstab.py
@@ -3,16 +3,14 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__)).replace('tests/', ''))
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)).replace("tests/", ""))
 from fstab import Fstab
-from cleep.libs.internals.cleepfilesystem import CleepFilesystem
-from cleep.exception import MissingParameter, InvalidParameter, CommandError
-from cleep.libs.tests.lib import TestLib
+from cleep.exception import MissingParameter
+from cleep.libs.tests.lib import TestLib, FileDescriptorMock
 import unittest
 import logging
-from pprint import pprint
-import io
-from unittest.mock import Mock
+from mock import Mock
 from cleep.libs.tests.common import get_log_level
 
 LOG_LEVEL = get_log_level()
@@ -20,8 +18,8 @@ LOG_LEVEL = get_log_level()
 
 class FstabTests(unittest.TestCase):
 
-    FILE_NAME = 'fstab'
-    CONTENT = u"""# /etc/fstab: static file system information.
+    FILE_NAME = "fstab"
+    CONTENT_STR = """# /etc/fstab: static file system information.
 #
 # Use 'blkid' to print the universally unique identifier for a
 # device; this may be used with UUID= as a more robust way to name devices
@@ -46,100 +44,165 @@ desktop:/data/stuff       /media/stuff       nfs     soft,rw,nfsvers=3,rsize=327
 192.168.1.1:/data/toserver       /media/toserver       nfs    soft,rw,nfsvers=3,rsize=32768,wsize=32768,timeo=600,actimeo=0,intr  0   0
 
 #media
-#192.168.1.10:/media/backup /media/tangmedia    nfs rsize=8192,wsize=8192,timeo=14,intr,vers=3
+#192.168.1.10:/media/backup /media/media    nfs rsize=8192,wsize=8192,timeo=14,intr,vers=3
 
 #store
-192.168.1.53:/media/raid    /media/tangstore    nfs     soft,rw,nfsvers=3,rsize=32768,wsize=32768,timeo=600,actimeo=0,intr  0   0"""
+192.168.1.53:/media/raid    /media/store    nfs     soft,rw,nfsvers=3,rsize=32768,wsize=32768,timeo=600,actimeo=0,intr  0   0"""
+
+    BLKID = {
+        "/dev/mmcblk0p1": {
+            "device": "/dev/mmcblk0p1",
+            "uuid": "EBBA-157F",
+            "type": "vfat",
+            "partuuid": "b294e190-01",
+        },
+        "/dev/mmcblk0p2": {
+            "device": "/dev/mmcblk0p2",
+            "uuid": "b3ce35cd-ade9-4755-a4bb-1571e37fc1b9",
+            "type": "ext4",
+            "partuuid": "b294e190-02",
+        },
+    }
 
     def setUp(self):
         TestLib()
-        logging.basicConfig(level=LOG_LEVEL, format=u'%(asctime)s %(name)s %(levelname)s : %(message)s')
+        logging.basicConfig(
+            level=LOG_LEVEL, format="%(asctime)s %(name)s %(levelname)s : %(message)s"
+        )
 
-        self.fs = CleepFilesystem()
-        self.fs.enable_write()
-        self.path = os.path.join(os.getcwd(), self.FILE_NAME)
-        logging.debug('Using conf file "%s"' % self.path)
+        self.fs = Mock()
+        self.fs.open.return_value = FileDescriptorMock(content=self.CONTENT_STR)
+        self.f = Fstab(self.fs, False)
 
-        #fill with default content
-        with io.open(self.path, 'w') as f:
-            f.write(self.CONTENT)
-
-        f = Fstab
-        f.CONF = self.FILE_NAME
-        self.f = f(self.fs, False)
-
-    def tearDown(self):
-        if os.path.exists('%s' % self.FILE_NAME):
-            os.remove('%s' % self.FILE_NAME)
+        blkid = Mock()
+        blkid.get_devices.return_value = self.BLKID
+        self.f.blkid = blkid
 
     def test_get_mountpoints(self):
         mountpoints = self.f.get_mountpoints()
         self.assertEqual(len(mountpoints), 9)
 
-        self.assertTrue('/media/usb1' in mountpoints)
-        self.assertEqual(mountpoints[u'/media/usb1'][u'local'], True)
-        self.assertTrue('/media/toserver' in mountpoints)
-        self.assertEqual(mountpoints[u'/media/toserver'][u'local'], False)
+        self.assertTrue("/media/usb1" in mountpoints)
+        self.assertEqual(mountpoints["/media/usb1"]["local"], True)
+        self.assertTrue("/media/toserver" in mountpoints)
+        self.assertEqual(mountpoints["/media/toserver"]["local"], False)
 
-    def test_devices(self):
+    def test_get_all_devices(self):
         devices = self.f.get_all_devices()
         self.assertNotEqual(len(devices), 0)
 
+    def test_add_mountpoint_invalid_parameters(self):
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            None,
+            "/dev/sda1",
+            "ext4",
+            "discard,noatime",
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "/media/mount",
+            None,
+            "ext4",
+            "discard,noatime",
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "/media/mount",
+            "/dev/sda1",
+            None,
+            "discard,noatime",
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "/media/mount",
+            "/dev/sda1",
+            "ext4",
+            None,
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "",
+            "/dev/sda1",
+            "ext4",
+            "discard,noatime",
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "/media/mount",
+            "",
+            "ext4",
+            "discard,noatime",
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "/media/mount",
+            "/dev/sda1",
+            "",
+            "discard,noatime",
+        )
+        self.assertRaises(
+            MissingParameter,
+            self.f.add_mountpoint,
+            "/media/mount",
+            "/dev/sda1",
+            "ext4",
+            "",
+        )
+
     def test_add_mountpoint(self):
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, None, u'/dev/sda1', u'ext4', u'discard,noatime')
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'/media/mount', None, u'ext4', u'discard,noatime')
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'/media/mount', u'/dev/sda1', None, u'discard,noatime')
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'/media/mount', u'/dev/sda1', u'ext4', None)
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'', u'/dev/sda1', u'ext4', u'discard,noatime')
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'/media/mount', u'', 'ext4', u'discard,noatime')
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'/media/mount', u'/dev/sda1', u'', u'discard,noatime')
-        self.assertRaises(MissingParameter, self.f.add_mountpoint, u'/media/mount', u'/dev/sda1', u'ext4', u'')
-
-        self.assertTrue(self.f.add_mountpoint('/dev/sda1', '/media/mount', 'ext4', 'discard,noatime'))
-
-        mountpoints = self.f.get_mountpoints()
-        self.assertTrue('/media/mount' in mountpoints)
+        self.f.add = Mock()
+        result = self.f.add_mountpoint("/dev/sda1", "/media/mount", "ext4", "discard,noatime")
+        self.assertTrue(result)
+        self.f.add.assert_called_with("\n/dev/sda1\t/media/mount\text4\tdiscard,noatime\t0\t0\n")
 
     def test_add_existing_mountpoint(self):
-        self.assertFalse(self.f.add_mountpoint('/dev/sda1', '/media/tangstore', 'ext4', 'discard,noatime'))
+        result = self.f.add_mountpoint("/dev/sdb1", "/media/usb1", "ext4", "discard,noatime")
+
+        self.assertFalse(result)
 
     def test_delete_unknown_mountpoint(self):
-        self.assertFalse(self.f.delete_mountpoint('/dev/dummy'))
+        result = self.f.delete_mountpoint("/dev/dummy")
+
+        self.assertFalse(result)
 
     def test_delete_mountpoint(self):
-        self.assertTrue(self.f.delete_mountpoint('/media/tangstore'))
-        mountpoints = self.f.get_mountpoints()
-        self.assertFalse('/media/tangstore' in mountpoints)
+        self.f.remove = Mock()
+
+        result = self.f.delete_mountpoint("/media/store")
+        
+        self.assertTrue(result)
+        self.f.remove.assert_called_with("192.168.1.53:/media/raid    /media/store    nfs     soft,rw,nfsvers=3,rsize=32768,wsize=32768,timeo=600,actimeo=0,intr  0   0")
 
     def test_delete_mountpoint_invalid_parameters(self):
         with self.assertRaises(MissingParameter) as cm:
             self.f.delete_mountpoint(None)
         self.assertEqual(cm.exception.message, 'Parameter "mountpoint" is missing')
+
         with self.assertRaises(MissingParameter) as cm:
-            self.f.delete_mountpoint('')
+            self.f.delete_mountpoint("")
         self.assertEqual(cm.exception.message, 'Parameter "mountpoint" is missing')
 
     def test_reload_fstab(self):
         self.f.console = Mock()
         self.f.console.command.return_value = {
-            'error': ['Error occured'],
-            'killed': False,
-        }
-        self.assertFalse(self.f.reload_fstab())
-
-        self.f.console.command.return_value = {
-            'error': [],
-            'killed': True,
-        }
-        self.assertFalse(self.f.reload_fstab())
-
-        self.f.console.command.return_value = {
-            'error': [],
-            'killed': False,
+            "returncode": 0
         }
         self.assertTrue(self.f.reload_fstab())
 
+        self.f.console.command.return_value = {
+            "returncode": 1,
+        }
+        self.assertFalse(self.f.reload_fstab())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # coverage run --omit="*/lib/python*/*","*test_*.py" --concurrency=thread test_fstab.py; coverage report -m -i
     unittest.main()
