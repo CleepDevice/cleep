961b2676ae7145c09198329fb155ce7a697702ff
Tanguy Bonneau <tanguy.bonneau@insurlytech.com>
2023-02-27 11:02:10 +0100

    Restore deleted html files

---
+++ html/js/app.configs.js
@@ -0,0 +1,172 @@
+/**
+ * Application modules configuration
+ * This file configures:
+ *  - router
+ *  - theme
+ *  - fonts provider
+ */
+
+var Cleep = angular.module('Cleep');
+var currentTimestamp = Date.now();
+
+/**
+ * Routes configuration
+ */
+Cleep
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('!');
+    $routeProvider
+        .when('/dashboard', {
+            template: '<div dashboard-directive></div>'
+        })
+        .when('/modules', {
+            template: '<div modules-directive></div>'
+        })
+        .when('/install', {
+            template: '<div install-directive></div>'
+        })
+        .when('/module/:name', {
+            template: '<div module-directive></div>'
+        })
+        .when('/module/:name/:page', {
+            template: '<div page-directive></div>'
+        })
+        .otherwise({
+            redirectTo: '/dashboard'
+        });
+}]);
+
+/**
+ * Disable aria warnings
+ */
+Cleep
+.config(function($mdAriaProvider) {
+    $mdAriaProvider.disableWarnings();
+});
+
+/**
+ * Http interceptor to resolve cache problems
+ */
+Cleep
+.config(function ($templateRequestProvider) {
+    $templateRequestProvider.httpOptions({ _isTemplate: true });
+})
+.factory('noCacheInterceptor', function($templateCache) {
+    return {
+        'request': function(config) {
+            if (config._isTemplate) {
+                return config;
+            }
+            if (config.url.indexOf('.html')!==-1 ||
+                config.url.indexOf('.json')!==-1 ||
+                config.url.indexOf('.svg')!==-1 ||
+                config.url.indexOf('.css')!==-1
+            ) {
+                config.url = config.url + '?t=' + currentTimestamp;
+            }
+            return config;
+        }
+    };
+})
+.config(function($httpProvider) {
+    $httpProvider.interceptors.push('noCacheInterceptor');
+});
+
+Cleep
+.factory('$exceptionHandler', ['$log', '$injector', function($log, $injector) {
+    return function myExceptionHandler(exception, cause) {
+        // classic log in console
+        $log.error(exception, cause);
+
+        if (typeof(exception) === 'string' && exception.startsWith('Possibly')) {
+            // should be already handled by a service
+            return;
+        }
+        var toastService = $injector.get('toastService');
+        var locationService = $injector.get('$location');
+        if (toastService && locationService && locationService.url().startsWith('/module/')) {
+            toastService.fatal('Failed to load '+ locationService.url().split('/').pop() +' application');
+        }
+    };
+}]);
+
+/**
+ * Theme configuration
+ */
+Cleep.config(['$mdThemingProvider', function($mdThemingProvider) {
+    $mdThemingProvider
+        .theme('default')
+        .primaryPalette('blue-grey')
+        .accentPalette('red')
+        .backgroundPalette('grey');
+    $mdThemingProvider
+        .theme('dark')
+        .primaryPalette('amber')
+        .accentPalette('blue')
+        .dark();
+
+    /*$mdThemingProvider
+        .theme('default')
+        .primaryPalette('blue')
+        .accentPalette('orange')
+        .backgroundPalette('grey');
+    $mdThemingProvider
+        .theme('dark')
+        .primaryPalette('blue')
+        .accentPalette('orange')
+        .backgroundPalette('grey')
+        .dark();*/
+}]);
+
+/**
+ * Font configuration
+ * Disabled for now, ligatures are not supported by typicons
+ */
+/*Cleep.config(['$mdIconProvider', function($mdIconProvider) {
+    $mdIconProvider
+        .iconSet('typicons', 'fonts/typicons.svg', 24)
+}]);*/
+
+/**
+ * MDI font configuration
+ */
+Cleep.config(['$mdIconProvider', function($mdIconProvider) {
+    $mdIconProvider.defaultIconSet('fonts/mdi.svg')
+}]);
+
+
+/**
+ * Blockui configuration
+ */
+Cleep.config(['blockUIConfig', function(blockUIConfig) {
+    tmpl =  '<div class="block-ui-overlay"></div>';
+    tmpl += '<div layout="column" layout-align="center center" class="block-ui-message-container">';
+    tmpl += '  <md-card style="display: inline-block; text-align: left;" md-colors="::{backgroundColor: \'default-primary-100\'}">';
+    tmpl += '    <md-card-title>';
+    tmpl += '      <md-card-title-media>';
+    tmpl += '        <div class="md-media-sm card-media" layout>';
+    tmpl += '          <md-icon ng-if="state.icon!==undefined && state.icon!==null" class="icon-xl" md-svg-icon="{{state.icon}}"></md-icon>';
+    tmpl += '          <md-progress-circular ng-if="state.spinner===undefined || state.spinner===true" md-mode="indeterminate" style="margin-top:14px; margin-left:10px;">';
+    tmpl += '        </div>';
+    tmpl += '      </md-card-title-media>';
+    tmpl += '      <md-card-title-text>';
+    tmpl += '        <span class="md-headline">{{state.message}}</span>';
+    tmpl += '        <span ng-if="state.submessage" class="md-subhead">{{state.submessage}}</span>';
+    tmpl += '      </md-card-title-text>';
+    tmpl += '    </md-card-title>';
+    tmpl += '  </md-card>';
+    tmpl += '</div>';
+    blockUIConfig.template = tmpl;
+    blockUIConfig.autoBlock = false;
+}]);
+
+/**
+ * Lazyload configuration
+ */
+Cleep.config(['$ocLazyLoadProvider', function($ocLazyLoadProvider) {
+    $ocLazyLoadProvider.config({
+       debug: false,
+       events: false
+    });
+}]);
+
---
+++ html/js/settings/install/install.js
@@ -0,0 +1,174 @@
+/**
+ * Configuration directive
+ * Handle all modules configuration
+ */
+var installDirective = function($q, cleepService, toast, $mdDialog, $sce) {
+
+    var installController = ['$rootScope', '$scope','$element', '$window', function($rootScope, $scope, $element, $window) {
+        var self = this;
+        self.cleepService = cleepService;
+        self.search = {'$': ''};
+        self.country = null;
+        self.countryAlpha = null;
+        self.moduleToInstall = null;
+        self.displayedModules = [];
+        self.gaugeThreshold = {
+            '0': { color: '#000000' },
+            '5': { color: '#d32f2f' },
+            '6': { color: '#f57c00' },
+            '7': { color: '#fbc02d' },
+            '8': { color: '#0288d1' },
+            '9': { color: '#689f38' },
+        };
+        self.moduleIncompatible = null;
+
+        /**
+         * Clear search input
+         */
+        self.clearSearch = function() {
+            self.search['$'] = '';
+        };
+
+        /**
+         * Install module
+         * @param module: module name (string)
+         */
+        self.install = function(module) {
+            // close modal
+            self.closeDialog();
+
+            // launch install
+            cleepService.installModule(module);
+        };
+
+        /**
+         * Fill modules list
+         */
+        self.fillModules = function() {
+            cleepService.getModuleConfig('parameters')
+            .then((parametersConfig) => {
+                // update list of modules names
+                var modules = [];
+                for( var [moduleName, module] of Object.entries(cleepService.installableModules) ) {
+                    // fix module country alpha code
+                    var countryAlpha = module.country;
+                    if( countryAlpha===null || countryAlpha===undefined ) {
+                        countryAlpha = '';
+                    }
+
+                    // append module if necessary
+                    if ((!module.installed || (module.installed && module.library)) &&
+                        (countryAlpha.length===0 || countryAlpha.toUpperCase()==parametersConfig.country.alpha2) ) {
+                        modules.push(module);
+                    }
+                }
+                self.displayedModules = modules;
+            });
+        };
+
+        /**
+         * Show incompatible dialog
+         */
+        self.showIncompatibleDialog = function(module, ev) {
+            self.moduleIncompatible = module;
+            $mdDialog.show({
+                controller: function() { return self; },
+                controllerAs: 'incompatibleCtl',
+                templateUrl: 'js/settings/install/incompatible.dialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: true
+            })
+            .then(function() {}, function() {});
+        };
+
+        /**
+         * Close update dialog
+         */
+        self.closeDialog = function() {
+            $mdDialog.hide();
+        };
+
+        /**
+         * Show install dialog
+         */
+        self.showInstallDialog = function(module, ev) {
+            self.moduleToInstall = module;
+
+            // trust html content
+            self.sceLongDescription = $sce.trustAsHtml(self.moduleToInstall.longdescription);
+            self.sceChangelog = $sce.trustAsHtml(self.moduleToInstall.changelog);
+
+            $mdDialog.show({
+                controller: function() { return self; },
+                controllerAs: 'installCtl',
+                templateUrl: 'js/settings/install/install.dialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev, 
+                clickOutsideToClose: true,
+                fullscreen: true
+            })  
+            .then(function() {}, function() {});
+        };
+
+        /**
+         * Redirect to update module logs page
+         */
+        self.gotoUpdateLogs = function() {
+            $window.location.href = '#!/module/update?tab=logs';
+        };
+
+        /** 
+         * Redirect to update module page
+         */
+        self.gotoUpdateCleep = function() {
+            self.closeDialog();
+            $window.location.href = '#!/module/update?tab=cleep';
+        };
+
+        /**
+         * Fill modules as soon as cleep configuration is loaded
+         */
+        $scope.$watch(
+            function() {
+                return cleepService.installableModules;
+            },
+            function(newValue, oldValue) {
+                if( newValue && Object.keys(newValue).length ) {
+                    self.fillModules();
+                }
+            },
+            true
+        );
+
+        /**
+         * Catch module install events
+         */
+        $rootScope.$on('update.module.install', function(event, uuid, params) {
+            // module install event received, refresh modules updates infos
+            cleepService.refreshModulesUpdates();
+        });
+
+        /**
+         * Init component
+         */
+        self.$onInit = function() {
+            // load mandatory data
+            cleepService.getInstallableModules();
+            cleepService.refreshModulesUpdates();
+        };
+
+    }];
+
+    return {
+        templateUrl: 'js/settings/install/install.html',
+        replace: true,
+        controller: installController,
+        controllerAs: 'installCtl',
+    };
+};
+
+var Cleep = angular.module('Cleep');
+Cleep.directive('installDirective', ['$q', 'cleepService', 'toastService', '$mdDialog', '$sce', installDirective]);
+
