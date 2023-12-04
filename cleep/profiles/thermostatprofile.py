961d3b2a7fb5e2a86d59432ab896a0395cb1ba68
tang <tanguy.bonneau@gmail.com>
2023-12-04 22:01:21 +0100

    Add thermostat profile

---
+++ cleep/profiles/thermostatprofile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cleep.libs.internals.rendererprofile import RendererProfile
+
+class ThermostatProfile(RendererProfile):
+    """
+    Thermostat profile
+    """
+
+    MODE_STOP = 'stop'
+    MODE_COMFORT1 = 'comfort1'
+    MODE_COMFORT2 = 'comfort2'
+    MODE_COMFORT3 = 'comfort3'
+    MODE_ECO = 'eco'
+    MODE_ANTIFROST = 'antifrost'
+
+    def __init__(self):
+        RendererProfile.__init__(self)
+        self.mode = None
+
